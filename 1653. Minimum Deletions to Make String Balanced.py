b3e1111d67fd714a2168ce7697aecb780b929eb1
amine charef <amineshx@shx>
2024-07-30 17:46:51 +0100

    Create 1653. Minimum Deletions to Make String Balanced.py

---
+++ 1653. Minimum Deletions to Make String Balanced.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        a_count_right=[0]*len(s)
+        for i in range(len(s)-2,-1,-1): #same thing as for i in reversed(range(len(s)-1))
+            a_count_right[i]=a_count_right[i+1]
+            a_count_right[i]+=1 if s[i+1]=='a'else 0
+        
+        b_count_left=0
+        res=len(s)
+
+        for i,c in enumerate(s):
+            res=min(res,b_count_left+a_count_right[i])
+            if c=='b':
+                b_count_left+=1
+        
+        return res
