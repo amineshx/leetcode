f2474c9618e53d865284dd8f5d9d56a70b89077f
amine charef <amineshx@shx>
2024-07-30 18:39:05 +0100

    Update 1653. Minimum Deletions to Make String Balanced.py

--- 1653. Minimum Deletions to Make String Balanced.py
+++ 1653. Minimum Deletions to Make String Balanced.py
@@ -14,3 +14,22 @@ class Solution:
                 b_count_left+=1
         
         return res
+
+#sol2:
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        a_count_right=0
+        for c in s: 
+            a_count_right +=1 if c=='a'else 0
+            
+        
+        b_count_left=0
+        res=len(s)
+        for i,c in enumerate(s):
+            if c=='a':
+                a_count_right-=1
+            res=min(res,b_count_left+a_count_right)
+            if c=='b':
+                b_count_left+=1
+        
+        return res
