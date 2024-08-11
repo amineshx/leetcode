2624d737816ef3b22d108b17bcca530d1bf45408
amineshx <amine.charef@etu.usthb.dz>
2024-08-11 14:39:27 +0100

    Update 1568. Minimum Number of Days to Disconnect Island.py

--- 1568. Minimum Number of Days to Disconnect Island.py
+++ 1568. Minimum Number of Days to Disconnect Island.py
@@ -1,4 +1,35 @@
 from typing import List
 class Solution:
     def minDays(self, grid: List[List[int]]) -> int:
+        rows,cols=len(grid), len(grid[0])
+
+        def DFS(r,c,visited):
+            if (r<0 or c< 0 or r==rows or c==cols or
+                grid[r][c]==0 or (r,c) in visited ):
+                return
+            visited.add((r,c))
+            neighbors=[[r+1,c],[r,c+1],[r-1,c],[r,c-1]]
+            for neiR, neiC in neighbors:
+                DFS(neiR,neiC,visited)
+        def Helper():
+            visited=set()
+            count=0
+            for row in range(rows):
+                for col in range(cols):
+                    if grid[row][col] and (row,col) not in visited:
+                        DFS(row,col,visited)
+                        count+=1
+            return count
         
+        if Helper() !=1:
+            return 0 
+        
+        for row in range(rows):
+            for col in range(cols): 
+                if grid[row][col]==0:
+                    continue
+                grid[row][col]=0
+                if Helper()!=1:
+                    return 1
+                grid[row][col]=1
+        return 2 
