ad546b4912e238b43c163950f17c72b1004b9f39
amineshx <amine.charef@etu.usthb.dz>
2024-08-16 17:52:43 +0100

    Update 624. Maximum Distance in Arrays.py

--- 624. Maximum Distance in Arrays.py
+++ 624. Maximum Distance in Arrays.py
@@ -2,4 +2,18 @@ from typing import List
 class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
         mins,maxs=[min(sub_array) for sub_array in arrays], [max(sub_array) for sub_array in arrays]
-        return abs(min(mins)-max(maxs))
+        Min=min(mins)
+        Min_index=mins.index(Min)
+        Max=max(maxs)
+        Max_index=maxs.index(Max)
+        if Min_index!=Max_index:
+            return Max-Min
+        else:
+            mins.sort()
+            maxs.sort()
+            prob=[maxs[-1]-mins[1],maxs[-2]-mins[0]]
+            return max(prob)
+            
+    
+sol= Solution()
+print(sol.maxDistance(arrays = [[1,2,3],[4,5],[1,2,3]]))
