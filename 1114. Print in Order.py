a9f9444b97155a762d6dd0ae8f213eeb3634ed61
amineshx <amine.charef@etu.usthb.dz>
2024-08-18 22:29:14 +0100

    Update 1114. Print in Order.py

--- 1114. Print in Order.py
+++ 1114. Print in Order.py
@@ -1,21 +1,19 @@
+import threading
+
 class Foo:
     def __init__(self):
-        pass
-
+        self.first_done = threading.Event()
+        self.second_done = threading.Event()
 
     def first(self, printFirst: 'Callable[[], None]') -> None:
-        
-        # printFirst() outputs "first". Do not change or remove this line.
         printFirst()
-
+        self.first_done.set()
 
     def second(self, printSecond: 'Callable[[], None]') -> None:
-        
-        # printSecond() outputs "second". Do not change or remove this line.
+        self.first_done.wait()
         printSecond()
-
+        self.second_done.set()
 
     def third(self, printThird: 'Callable[[], None]') -> None:
-        
-        # printThird() outputs "third". Do not change or remove this line.
+        self.second_done.wait()
         printThird()
