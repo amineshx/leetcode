da2879bb1913dfe5012aad3ba245f30d4e84e19d
amineshx <amine.charef@etu.usthb.dz>
2024-08-18 13:51:04 +0100

    Update 264. Ugly Number II.py with a backtracking solution with minHeap

--- 264. Ugly Number II.py
+++ 264. Ugly Number II.py
@@ -1,3 +1,17 @@
+import heapq
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        
+        minHeap=[1] # init the minheap with the first ugly num which is 1
+        visited=set()
+        prim_factors=[2,3,5]    
+
+        for i in range(n):
+            res=heapq.heappop(minHeap)
+            if i == n-1:
+                return res
+            
+            for j in  prim_factors:
+                num= res*j
+                if num not in visited:
+                    visited.add(num)
+                    heapq.heappush(minHeap,num)
