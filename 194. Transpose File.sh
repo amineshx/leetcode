fb91753dda392b5e447330f3738ee3e1358afeab
amineshx <amine.charef@etu.usthb.dz>
2024-08-29 16:28:28 +0100

    Create 194. Transpose File.sh

---
+++ 194. Transpose File.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Read from the file file.txt and print its transposed content to stdout.
+awk '{
+    for (i=1; i<=NF; i++) {
+        if (NR==1) {
+            arr[i]=$i
+        } else {
+            arr[i]=arr[i] " " $i}
+        }
+    } 
+    END {
+        for (i=1; i<=NF; i++) {
+            print arr[i]
+        }
+    }' file.txt
