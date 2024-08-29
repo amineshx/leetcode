e78a5d820a5f3546afd0dd69580902fa84306f0f
amineshx <amine.charef@etu.usthb.dz>
2024-08-29 16:09:10 +0100

    Update 193. Valid Phone Numbers.sh

--- 193. Valid Phone Numbers.sh
+++ 193. Valid Phone Numbers.sh
@@ -8,3 +8,26 @@ while IFS= read -r line; do
         echo "$line"
     fi
 done < file.txt
+
+
+#sol 2
+
+
+#!/bin/bash
+
+# Define the regex pattern for valid phone numbers
+pattern='^([0-9]{3}-[0-9]{3}-[0-9]{4}|\([0-9]{3}\) [0-9]{3}-[0-9]{4})$'
+
+# Read the file line by line and filter valid phone numbers
+while IFS= read -r line; do
+    # Use regex to check if the line matches the valid phone number pattern
+    [[ $line =~ $pattern ]] && echo "$line"
+done < file.txt
+
+
+
+#sol 3  the best and the simplest and optimised one
+
+#!/bin/bash
+
+grep -E '^[0-9]{3}-[0-9]{3}-[0-9]{4}$|^\([0-9]{3}\) [0-9]{3}-[0-9]{4}$' file.txt
