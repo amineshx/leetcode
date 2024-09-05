885e5cccc0d868787e4b4bb4eb064d02087e561a
amineshx <amine.charef@etu.usthb.dz>
2024-09-05 17:18:38 +0100

    Update 2028. Find Missing Observations

--- 2028. Find Missing Observations.py
+++ 2028. Find Missing Observations.py
@@ -1,4 +1,17 @@
 from typing import List
 class Solution:
     def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        m = len(rolls)
+        total_sum=mean*(m+n)
+        missing_sum=total_sum-sum(rolls)
+        res=[]
+
+        if missing_sum > 6*n or missing_sum <n:
+            return []
         
+        while n:
+            dice = min(6,missing_sum-n+1)
+            res.append(dice)
+            missing_sum-= dice
+            n-=1
+        return res
