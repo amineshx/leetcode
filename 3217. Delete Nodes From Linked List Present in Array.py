52ee9364f3750f3476a5ef655923ad33a3922931
amineshx <amine.charef@etu.usthb.dz>
2024-09-06 22:30:20 +0100

    Update 3217. Delete Nodes From Linked List Present in Array

---
+++ 3217. Delete Nodes From Linked List Present in Array.py
@@ -0,0 +1,9 @@
+# Definition for singly-linked list.
+from typing import Optional,List
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+        
