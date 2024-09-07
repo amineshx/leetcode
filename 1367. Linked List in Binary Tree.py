cf4fd76c4b204a12ba9e8557700bdec6e618d5a4
amineshx <amine.charef@etu.usthb.dz>
2024-09-07 23:34:36 +0100

    Update 1367. Linked List in Binary Tree

--- 1367. Linked List in Binary Tree.py
+++ 1367. Linked List in Binary Tree.py
@@ -13,3 +13,22 @@ class TreeNode:
 class Solution:
     def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
         
+        
+        if self.helper(head,root):
+            return True
+        if not root:
+            return False
+        return(
+            self.isSubPath(head, root.left) or 
+            self.isSubPath(head, root.right)
+        )
+    
+    def helper(self,list_node, tree_node):
+            if not list_node:
+                return True
+            if not tree_node or list_node.val!=tree_node.val:
+                return False
+            return(
+                self.helper(list_node.next, tree_node.left) or 
+                self.helper(list_node.next, tree_node.right)
+            )
