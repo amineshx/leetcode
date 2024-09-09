1929276bc42e692f05d3b265f6ee809fc8f3c463
amineshx <amine.charef@etu.usthb.dz>
2024-09-09 22:41:59 +0100

    Update 2326. Spiral Matrix IV

---
+++ 2326. Spiral Matrix IV.py
@@ -0,0 +1,9 @@
+# Definition for singly-linked list.
+from typing import List, Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
+        
