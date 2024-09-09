88765b3102bc7f37f90e19bedd6d685880eda150
amineshx <amine.charef@etu.usthb.dz>
2024-09-09 23:15:34 +0100

    Update 2326. Spiral Matrix IV

--- 2326. Spiral Matrix IV.py
+++ 2326. Spiral Matrix IV.py
@@ -6,4 +6,36 @@ class ListNode:
         self.next = next
 class Solution:
     def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
+        left,right,top,bottom = 0,n,0,m 
+        res = [[-1]*n for _ in range(m)]
+
+        while head :
+            for i in range(left,right):
+                if not head :
+                    return res
+                res[top][i]=head.val
+                head = head.next
+            top+=1
+
+            for i in range(top,bottom):
+                if not head :
+                    return res
+                res[i][right-1]=head.val
+                head = head.next
+            right-=1
+
+            for i in range(right-1,left-1,-1):
+                if not head :
+                    return res
+                res[bottom-1][i]=head.val
+                head = head.next
+            bottom-=1
+
+            for i in range(bottom-1,top-1,-1):
+                if not head :
+                    return res
+                res[i][left]=head.val
+                head = head.next
+            left+=1
         
+        return res
