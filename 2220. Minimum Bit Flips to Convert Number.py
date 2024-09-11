ea94b0453cdeffbac8168db40a72f6638ada23d7
amineshx <amine.charef@etu.usthb.dz>
2024-09-11 14:37:21 +0100

    Update 2220. Minimum Bit Flips to Convert Number

--- 2220. Minimum Bit Flips to Convert Number.py
+++ 2220. Minimum Bit Flips to Convert Number.py
@@ -1,3 +1,10 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
-        
+       
+        differing_bits = start ^ goal
+        print(differing_bits)
+        # Count the number of 1's in the binary representation of differing_bits
+        return bin(differing_bits).count('1')
+
+sol = Solution()
+print(sol.minBitFlips(10, 7))
