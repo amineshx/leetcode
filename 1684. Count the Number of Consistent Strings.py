83d56d1bd4b18df9038577ded60ca60d1d06a17b
amineshx <amine.charef@etu.usthb.dz>
2024-09-12 15:50:35 +0100

    Update 1684. Count the Number of Consistent Strings with bruteforce solution

--- 1684. Count the Number of Consistent Strings.py
+++ 1684. Count the Number of Consistent Strings.py
@@ -1,4 +1,14 @@
 from typing import List
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
-        
+        res =0 
+        allowed=tuple(allowed)
+        for word in words:
+            check=True
+            for c in word:
+                if c not in allowed:
+                    check=False
+                    break
+            if check==True:
+                res+=1 
+        return res
