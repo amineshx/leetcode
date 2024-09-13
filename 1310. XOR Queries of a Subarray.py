2b695797f6e62091f9a45df4017d410c20ffad18
amineshx <amine.charef@etu.usthb.dz>
2024-09-13 18:48:10 +0100

    Update 1310. XOR Queries of a Subarray with prefix xor solution

--- 1310. XOR Queries of a Subarray.py
+++ 1310. XOR Queries of a Subarray.py
@@ -1,11 +1,27 @@
 from typing import List
 
+# class Solution:
+#     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+#         res = []
+#         for start, end in queries:
+#             subres = 0  
+#             for i in range(start, end + 1):
+#                 subres ^= arr[i]
+#             res.append(subres)
+#         return res
+
+
+# sol2
+
 class Solution:
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
-        res = []
-        for start, end in queries:
-            subres = 0  
-            for i in range(start, end + 1):
-                subres ^= arr[i]
-            res.append(subres)
+        prefix=[0]
+        for n in arr:
+            prefix.append(prefix[-1]^n)
+        
+        res=[]
+        for left,right in queries:
+            res.append(
+                prefix[left]^prefix[right+1]
+            )
         return res
