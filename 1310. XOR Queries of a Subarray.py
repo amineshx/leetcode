8732ec8394987ee5a965e60347e11580c1f66b48
amineshx <amine.charef@etu.usthb.dz>
2024-09-13 18:40:01 +0100

    Update 1310. XOR Queries of a Subarray with brute force solution , its not afficant

--- 1310. XOR Queries of a Subarray.py
+++ 1310. XOR Queries of a Subarray.py
@@ -1,4 +1,11 @@
 from typing import List
+
 class Solution:
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
-        
+        res = []
+        for start, end in queries:
+            subres = 0  
+            for i in range(start, end + 1):
+                subres ^= arr[i]
+            res.append(subres)
+        return res
