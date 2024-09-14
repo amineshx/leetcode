dd86f1631365f1276cf2322e44d4ef2c5c9ab606
amineshx <amine.charef@etu.usthb.dz>
2024-09-14 18:38:15 +0100

    Update 2419. Longest Subarray With Maximum Bitwise AND with linear space and time  solution

--- 2419. Longest Subarray With Maximum Bitwise AND.py
+++ 2419. Longest Subarray With Maximum Bitwise AND.py
@@ -1,10 +1,30 @@
 from typing import List
+# class Solution:
+#     def longestSubarray(self, nums: List[int]) -> int:
+#         target = max(nums)
+#         size,res = 0,0
+#         for n in nums:
+#             if n == target:
+#                 size+=1
+#             else:
+#                 size=0
+#             res = max(res,size)
+#         return res
+    
+
+
+#sol 2
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        target = max(nums)
         size,res = 0,0
+        current_max = 0
+
         for n in nums:
-            if n == target:
+            if n>current_max:
+                current_max=n
+                size=1
+                res=0
+            elif n==current_max:
                 size+=1
             else:
                 size=0
