aee81475493cdbefa284cb72036d0169f082ca34
amineshx <amine.charef@etu.usthb.dz>
2024-09-15 22:42:14 +0100

    Update 1371. Find the Longest Substring Containing Vowels in Even Counts with sol1

--- 1371. Find the Longest Substring Containing Vowels in Even Counts.py
+++ 1371. Find the Longest Substring Containing Vowels in Even Counts.py
@@ -1,3 +1,15 @@
 class Solution:
     def findTheLongestSubstring(self, s: str) -> int:
-        
+        vowles = "aeiou"
+        res,mask,mask_to_index =0,0,{0:-1}
+
+
+        for i,c in enumerate(s):
+            if c in vowles:
+                mask = mask^(1+ord(c)-ord('a'))
+            if mask in mask_to_index :
+                length = i-mask_to_index[mask]
+                res = max(res,length)
+            else:
+                mask_to_index[mask]=i
+        return res
