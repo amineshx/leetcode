6655c65f78a807438b6f85176fe7a085e6949959
amineshx <amine.charef@etu.usthb.dz>
2024-09-16 21:30:39 +0100

    Update 539. Minimum Time Difference with sol1

--- 539. Minimum Time Difference.py
+++ 539. Minimum Time Difference.py
@@ -1,4 +1,19 @@
 from typing import List
 class Solution:
     def findMinDifference(self, timePoints: List[str]) -> int:
+        timePoints.sort()
         
+        res = (
+            24*60 - self.get_time(timePoints[-1])+self.get_time(timePoints[0])
+        )
+        for i in range(len(timePoints)-1):
+            cur = self.get_time(timePoints[i+1])
+            prev = self.get_time(timePoints[i])
+            diff = cur-prev
+            res = min(res,diff)
+        return res
+    
+    def get_time(self,t):
+        HH,MM=map(int,t.split(":"))
+
+        return 60*HH + MM
