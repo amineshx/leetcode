c55bf363b8e4bdcb8e02b8d97fd0dc3028467a7b
amineshx <amine.charef@etu.usthb.dz>
2024-09-18 20:12:33 +0100

    Update 179. Largest Number.py

--- 179. Largest Number.py
+++ 179. Largest Number.py
@@ -1,4 +1,18 @@
 from typing import List
+from functools import cmp_to_key
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         
+        for i, num in enumerate(nums):
+            nums[i] = str(num)
+        
+        nums = sorted(nums,key=cmp_to_key(self.compare))
+        return str(int("".join(nums)))
+
+    
+    def compare(self,num1,num2):
+        if num1+num2>num2+num1:
+            return -1
+        else:
+            return 1
+    
