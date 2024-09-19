d8a7492aef0375e97615eb15f37ab7dde75b3492
amineshx <amine.charef@etu.usthb.dz>
2024-09-19 16:37:57 +0100

    Update 241. Different Ways to Add Parentheses

--- 241. Different Ways to Add Parentheses.py
+++ 241. Different Ways to Add Parentheses.py
@@ -29,3 +29,29 @@ class Solution:
         if res==[]:
             res.append(int(self.expression[left:right+1]))
         return res
+
+class Solution:
+    operations = {
+        "+": lambda x,y:x+y,
+        "-": lambda x,y:x-y,
+        "*": lambda x,y:x*y,
+    }
+
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        
+        res=[]
+
+        for i in range(len(expression)):
+            op = expression[i]
+            if op in self.operations:
+                nums1=self.diffWaysToCompute(expression[:i])
+                nums2=self.diffWaysToCompute(expression[i+1:])
+
+                for n1 in nums1:
+                    for n2 in nums2:
+                        res.append(self.operations[op](n1,n2))
+        if res==[]:
+            res.append(int(expression))
+        return res
+        
+    
