2e8822e274082440a21e3bfd91e302dd357f7a0e
amineshx <amine.charef@etu.usthb.dz>
2024-09-22 18:49:47 +0100

    Update 440. K-th Smallest in Lexicographical Order.py memory Limit Exceeded 'another failed attempte'

--- 440. K-th Smallest in Lexicographical Order.py
+++ 440. K-th Smallest in Lexicographical Order.py
@@ -1,8 +1,11 @@
 # not optimezed solution
 class Solution:
     def findKthNumber(self, n: int, k: int) -> int:
-        res= sorted([c for c in range(1, n+1)], key=str)
-        return res[k-1]
+        res = []
+        for i, c in enumerate(sorted(range(1, n+1), key=str), start=1):
+            res.append(c)
+            if i == k:
+                return c
 
 sol = Solution()
 print(sol.findKthNumber(n=13,k=2))
