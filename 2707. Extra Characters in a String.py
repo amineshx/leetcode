a91d46550c9db47f67184d560a68613c7586d636
amineshx <amine.charef@etu.usthb.dz>
2024-09-23 19:49:02 +0100

    Update 2707. Extra Characters in a String with DP solution num2

--- 2707. Extra Characters in a String.py
+++ 2707. Extra Characters in a String.py
@@ -16,3 +16,22 @@ class Solution:
                     dp[i] = min(dp[i], dp[j + 1])
         
         return dp[0]
+
+#DP solution2
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        word_set = set(dictionary)
+        dp={ len(s):0 }
+
+        def DFS(i):
+            if i in dp:
+                return dp[i]
+            
+            res = 1+ DFS(i+1)
+            for j in range(i,len(s)):
+                if s[i:j+1] in word_set:
+                    res = min(res, DFS(j+1))
+            dp[i]=res
+            return res
+        
+        return DFS(0)
