4bef4ccfdf151ed9aeba95ca2399d05ff8d94762
amineshx <amine.charef@etu.usthb.dz>
2024-09-26 23:09:28 +0100

    Update 729. My Calender I.py with Updating the second solution and adding function to execute the code

--- 729. My Calendar I.py
+++ 729. My Calendar I.py
@@ -16,35 +16,56 @@ class MyCalendar:
 
 #second sol
 class Tree:
-    def __init__(self,start,end):
-        self.left=None
-        self.right= None
-        self.start=start
-        self.end=end
+    def __init__(self, start, end):
+        self.left = None
+        self.right = None
+        self.start = start
+        self.end = end
 
     def insertion(self, start, end):
-        cur = self
-        while True:
-            if start >= cur.end:
-                if not cur.right:
-                    cur.right=Tree(start,end)
-                    return True
-                cur = cur.right
-            elif end <= cur.start:
-                if not cur.left:
-                    cur.left = Tree(start,end)
-                    return True
-                cur=cur.left
-            else:
-                return False
+        if start >= self.end:  
+            if not self.right: 
+                self.right = Tree(start, end)
+                return True
+            return self.right.insertion(start, end) 
+        elif end <= self.start:  
+            if not self.left:  
+                self.left = Tree(start, end)
+                return True
+            return self.left.insertion(start, end)  
+        else:
+            return False  
+
 
 class MyCalendar:
 
     def __init__(self):
-        self.root=None
+        self.root = None
 
     def book(self, start: int, end: int) -> bool:
-        if not self.root:
-            self.root=Tree(start,end)
-            return Tree
-        return self.root.insertion(start,end)
+        if not self.root:  
+            self.root = Tree(start, end)
+            return True  
+        return self.root.insertion(start, end)
+
+
+def simulate_my_calendar(commands, inputs):
+    output = []
+    myCalendar = None
+
+    for i, command in enumerate(commands):
+        if command == "MyCalendar":
+            myCalendar = MyCalendar()
+            output.append(None)  
+        elif command == "book":
+            start, end = inputs[i]
+            result = myCalendar.book(start, end)
+            output.append(result)  
+
+    return output
+
+
+commands = ["MyCalendar", "book", "book", "book"]
+inputs = [[], [10, 20], [15, 25], [20, 30]]
+output = simulate_my_calendar(commands, inputs)
+print(output)
