b46aa17d473cd3b6683c70e59aaccb4951331260
amineshx <amine.charef@etu.usthb.dz>
2024-09-26 23:00:27 +0100

    Update 729. My Calender I.py with second solution

--- 729. My Calendar I.py
+++ 729. My Calendar I.py
@@ -1,3 +1,4 @@
+# first sol
 class MyCalendar:
 
     def __init__(self):
@@ -10,3 +11,40 @@ class MyCalendar:
         
         self.events.append((start,end))
         return True
+
+
+
+#second sol
+class Tree:
+    def __init__(self,start,end):
+        self.left=None
+        self.right= None
+        self.start=start
+        self.end=end
+
+    def insertion(self, start, end):
+        cur = self
+        while True:
+            if start >= cur.end:
+                if not cur.right:
+                    cur.right=Tree(start,end)
+                    return True
+                cur = cur.right
+            elif end <= cur.start:
+                if not cur.left:
+                    cur.left = Tree(start,end)
+                    return True
+                cur=cur.left
+            else:
+                return False
+
+class MyCalendar:
+
+    def __init__(self):
+        self.root=None
+
+    def book(self, start: int, end: int) -> bool:
+        if not self.root:
+            self.root=Tree(start,end)
+            return Tree
+        return self.root.insertion(start,end)
