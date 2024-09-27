bcd394db06b79fab2a955b4101abd63044a3e652
amineshx <amine.charef@etu.usthb.dz>
2024-09-27 17:46:43 +0100

    Update 731. My Calendar II

--- 731. My Calendar II.py
+++ 731. My Calendar II.py
@@ -1,6 +1,17 @@
 class MyCalendarTwo:
 
     def __init__(self):
-        
+        self.intersection=[]
+        self.non_intersecton=[]
 
     def book(self, start: int, end: int) -> bool:
+        for i,j in self.intersection:
+            if end > i and start < j :
+                return False
+        
+        for i,j in self.non_intersecton:
+            if end > i and start < j :
+                interval = (max(i,start), min(j,end))
+                self.intersection.append(interval)
+        self.non_intersecton.append((start,end))
+        return True
