6be42a915c713781861c65f5e9725fedc897bd0f
amineshx <amine.charef@etu.usthb.dz>
2024-09-27 17:50:36 +0100

    Update 731. My Calendar II executing the code and printing solution

--- 731. My Calendar II.py
+++ 731. My Calendar II.py
@@ -15,3 +15,25 @@ class MyCalendarTwo:
                 self.intersection.append(interval)
         self.non_intersecton.append((start,end))
         return True
+
+
+def simulate_my_calendar(commands, inputs):
+    output = []
+    myCalendar = None
+
+    for i, command in enumerate(commands):
+        if command == "MyCalendarTwo":
+            myCalendar = MyCalendarTwo()
+            output.append(None)  
+        elif command == "book":
+            start, end = inputs[i]
+            result = myCalendar.book(start, end)
+            output.append(result)  
+
+    return output
+
+
+commands = ["MyCalendarTwo","book","book","book","book","book","book"]
+inputs = [[],[10,20],[50,60],[10,40],[5,15],[5,10],[25,55]]
+output = simulate_my_calendar(commands, inputs)
+print(output)
