3d1dbe06f64f741c6cda5ae2f1e8423b59a40c64
amineshx <amine.charef@etu.usthb.dz>
2024-10-01 23:16:48 +0100

    uPDate 1497. Check If Array Pairs Are Divisible by k.py with a solution that not satisfied all cases

--- 1497. Check If Array Pairs Are Divisible by k/1497. Check If Array Pairs Are Divisible by k.py
+++ 1497. Check If Array Pairs Are Divisible by k/1497. Check If Array Pairs Are Divisible by k.py
@@ -1,5 +1,19 @@
 from typing import List
 class Solution:
     def canArrange(self, arr: List[int], k: int) -> bool:
-        
+        freq = [((x%k)+k)%k for x in arr]
+        n=len(arr)
+        left = 0
+        right = n-1
+        while left < right:
+            if freq[left]+freq[right]%k ==0:
+                if right-left==1 and freq[right]+freq[left]!=0:
+                    return False
+            left+=1
+            right-=1
+                
+        return True
+
+sol = Solution()
+print(sol.canArrange(arr = [-1,1,-2,2,-3,3,-4,4], k = 3))
         
