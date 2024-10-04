5f48c7a3e50067ba2603fb8a053473e61cdc9d5a
amineshx <amine.charef@etu.usthb.dz>
2024-10-04 12:59:33 +0100

    Update 1590. Make Sum Divisible by P

--- 1590. Make Sum Divisible by P/1590. Make Sum Divisible by P.py
+++ 1590. Make Sum Divisible by P/1590. Make Sum Divisible by P.py
@@ -1,4 +1,20 @@
 from typing import List
 class Solution:
     def minSubarray(self, nums: List[int], p: int) -> int:
+        total = sum(nums)
+        remainder = total%p
+
+        if remainder == 0 :
+            return 0
         
+        res = len(nums)
+        current_sum = 0
+        remainder_to_index = {0:-1}
+        for i, n in enumerate(nums):
+            current_sum = (current_sum+n)%p
+            prefix = (current_sum-remainder+p)%p
+            if prefix in remainder_to_index:
+                length = i -remainder_to_index[prefix]
+                res= min(res, length)
+            remainder_to_index[current_sum]=i
+        return -1 if res== len(nums) else res
