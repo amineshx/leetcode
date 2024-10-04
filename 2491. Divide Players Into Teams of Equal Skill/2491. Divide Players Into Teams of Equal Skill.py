8509b94a3ad4e1686d7c67acd5bf3ff135fb6eec
amineshx <amine.charef@etu.usthb.dz>
2024-10-04 23:20:10 +0100

    Update 2491. Divide Players Into Teams of Equal Skill

--- 2491. Divide Players Into Teams of Equal Skill/2491. Divide Players Into Teams of Equal Skill.py
+++ 2491. Divide Players Into Teams of Equal Skill/2491. Divide Players Into Teams of Equal Skill.py
@@ -1,4 +1,22 @@
 from typing import List
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
+        if len(skill)==2:
+            return skill[0]*skill[1]
         
+        skill.sort()
+        n=len(skill)
+        target = skill[0]+skill[n-1]
+        res = skill[0]*skill[n-1]
+        
+        left,right=1,n-2
+        while left<right:
+            summ = skill[left]+skill[right]
+            if summ != target:
+                return -1
+            else :
+                res+= skill[left]*skill[right]
+            
+            left+=1
+            right-=1
+        return res
