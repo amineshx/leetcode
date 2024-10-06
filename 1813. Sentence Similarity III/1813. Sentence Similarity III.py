4e2bb7a4ac43c84a1c2a8dce93dabd51ae5b826f
amineshx <amine.charef@etu.usthb.dz>
2024-10-06 17:53:06 +0100

    Update 1813. Sentence Similarity III

--- 1813. Sentence Similarity III/1813. Sentence Similarity III.py
+++ 1813. Sentence Similarity III/1813. Sentence Similarity III.py
@@ -1,3 +1,17 @@
 class Solution:
     def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
+        s1 = sentence1.split(" ")
+        s2 = sentence2.split(" ")
+
+        if len(s1)> len(s2):
+            s1,s2 = s2,s1
         
+        left1 , left2 =0,0
+        while left1 < len(s1) and left2 < len(s2) and s1[left1] == s2[left2] :
+            left1 , left2 = left1 +1 , left2 +1
+
+        right1 , right2 = len(s1)-1 , len(s2)-1
+        while right1 >=0 and right2 >= 0 and s1[right1]==s2[right2]:
+            right1,right2 = right1-1, right2-1
+        
+        return left1 > right1
