befde124b1235c3a1d19c0fa04eccb8f96eceb36
amineshx <amine.charef@etu.usthb.dz>
2024-10-07 23:34:46 +0100

    Update 2696. Minimum String Length After Removing Substrings with a stack solution

--- 2696. Minimum String Length After Removing Substrings/2696. Minimum String Length After Removing Substrings.py
+++ 2696. Minimum String Length After Removing Substrings/2696. Minimum String Length After Removing Substrings.py
@@ -11,5 +11,16 @@ class Solution:
                 i+=1
         return len(s)
 
+# sol2 using a stack
+class Solution:
+    def minLength(self, s: str) -> int:
+        stack = []
+
+        for char in s:
+            stack.append(char)
+            if len(stack)>=2 and ((stack[-2]=='A' and stack[-1]=='B') or (stack[-2]=='C' and stack[-1]=='D')):
+                stack.pop()
+                stack.pop()
+        return len(stack)
 sol=Solution()
 print(sol.minLength( s = "ABFCACDB"))
