cf6dee1cef68380cd34baa566070056d2094762d
amineshx <amine.charef@etu.usthb.dz>
2024-10-07 23:24:07 +0100

    Update 2696. Minimum String Length After Removing Substrings with solution with memory Limit Exceeded

--- 2696. Minimum String Length After Removing Substrings/2696. Minimum String Length After Removing Substrings.py
+++ 2696. Minimum String Length After Removing Substrings/2696. Minimum String Length After Removing Substrings.py
@@ -1,4 +1,15 @@
 class Solution:
     def minLength(self, s: str) -> int:
         i = 0
-        while i<len(s)
+        while i<len(s)-1:
+            if (s[i]=='A' and s[i+1]=='B') or (s[i]=='C' and s[i+1]=='D'):
+                s = s[:i]+s[i+2:]
+                i-=1
+                print("s ",s)
+                print("i ",i)
+            else:
+                i+=1
+        return len(s)
+
+sol=Solution()
+print(sol.minLength( s = "ABFCACDB"))
