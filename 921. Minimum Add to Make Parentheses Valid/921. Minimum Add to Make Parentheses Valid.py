c97e1e20a5c5be33b1f73c31abe9d5049cace515
amineshx <amine.charef@etu.usthb.dz>
2024-10-09 23:50:43 +0100

    Update 921. Minimum Add to Make Parentheses Valid

--- 921. Minimum Add to Make Parentheses Valid/921. Minimum Add to Make Parentheses Valid.py
+++ 921. Minimum Add to Make Parentheses Valid/921. Minimum Add to Make Parentheses Valid.py
@@ -8,3 +8,17 @@ class Solution:
             else :
                 count -=1
         return abs(count)
+
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        open_parentesis = 0
+        res = 0
+
+        for c in s:
+            if c =="(":
+                open_parentesis +=1
+            else :
+                if open_parentesis==0:
+                    res+=1
+                open_parentesis = max(open_parentesis-1,0)
+        return res + open_parentesis
