86c7c6b3189a0d5c9330a5036728b26ee6ddc3e1
amineshx <amine.charef@etu.usthb.dz>
2024-10-09 23:46:46 +0100

    Update 921. Minimum Add to Make Parentheses Valid didn't pass all test cases

--- 921. Minimum Add to Make Parentheses Valid/921. Minimum Add to Make Parentheses Valid.py
+++ 921. Minimum Add to Make Parentheses Valid/921. Minimum Add to Make Parentheses Valid.py
@@ -1,3 +1,10 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        
+        #stack = []
+        count = 0
+        for c in s :
+            if c=='(':
+                count+=1
+            else :
+                count -=1
+        return abs(count)
