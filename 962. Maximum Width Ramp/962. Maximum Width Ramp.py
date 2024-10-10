01360589bc82ab8f5f1259412d97308187f0bc54
amineshx <amine.charef@etu.usthb.dz>
2024-10-10 19:43:22 +0100

    Update 962. Maximum Width Ramp with sliding window solution

--- 962. Maximum Width Ramp/962. Maximum Width Ramp.py
+++ 962. Maximum Width Ramp/962. Maximum Width Ramp.py
@@ -1,4 +1,23 @@
 from typing import List 
 class Solution:
     def maxWidthRamp(self, nums: List[int]) -> int:
+        n=len(nums)
+        max_right = [0]*n
+        i = n-1
+        prev_max=0
+
+        for num in reversed(nums):
+            max_right[i]=max(num,prev_max)
+            prev_max = max_right[i]
+            i-=1
         
+        res =0 
+        left = 0
+        for right in range(n):
+            while nums[left] > max_right[right]:
+                left+=1
+            res = max(res,right-left)
+        return res 
+
+sol = Solution()
+print(sol.maxWidthRamp(nums = [6,0,8,2,1,5]))
