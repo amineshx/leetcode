44013b4f4b60858b2edaf2a0ae9b9f1041051ded
amineshx <amine.charef@etu.usthb.dz>
2024-10-11 21:09:01 +0100

    Update 1942. The Number of the Smallest Unoccupied Chair

--- 1942. The Number of the Smallest Unoccupied Chair/1942. The Number of the Smallest Unoccupied Chair.py
+++ 1942. The Number of the Smallest Unoccupied Chair/1942. The Number of the Smallest Unoccupied Chair.py
@@ -16,6 +16,23 @@ class Solution:
             if i == targetFriend:
                 return chair
 
+class Solution:
+    def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
+        indexes = [i for i in range(len(times))]
+        indexes.sort(key=lambda i : times[i][0])
 
+        used_chairs = []
+        avaliable_chairs = [i for i in range(len(times))]
+
+        for i in indexes:
+            a,l = times[i]
+            while used_chairs and used_chairs[0][0] <=a:
+                leave, chair = heapq.heappop(used_chairs)
+                heapq.heappush(avaliable_chairs,chair)
+            chair = heapq.heappop(avaliable_chairs)
+            heapq.heappush(used_chairs,(l,chair))
+            if i == targetFriend:
+                return chair
+            
 sol = Solution()
 print(sol.smallestChair(times = [[1,4],[2,3],[4,6]], targetFriend = 1))
