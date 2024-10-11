129880b647c88fde512cee6adc24edbad4c36337
amineshx <amine.charef@etu.usthb.dz>
2024-10-11 20:58:17 +0100

    Update 1942. The Number of the Smallest Unoccupied Chair

--- 1942. The Number of the Smallest Unoccupied Chair/1942. The Number of the Smallest Unoccupied Chair.py
+++ 1942. The Number of the Smallest Unoccupied Chair/1942. The Number of the Smallest Unoccupied Chair.py
@@ -1,4 +1,12 @@
 from typing import List
 class Solution:
     def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
-        
+        times = [ (time[0],time[1],i) for i,time in enumerate(times)]
+        times.sort()
+        used_chairs = []
+        avaliable_chairs = [i for i in range(len(times))]
+
+        return avaliable_chairs
+
+sol = Solution()
+print(sol.smallestChair(times = [[1,4],[2,3],[4,6]], targetFriend = 1))
