1933ea0543787940ab40c0ed3cc917b011d5697c
amineshx <amine.charef@etu.usthb.dz>
2024-10-13 00:24:07 +0100

    Update 2406. Divide Intervals Into Minimum Number of Groups

--- 2406. Divide Intervals Into Minimum Number of Groups/2406. Divide Intervals Into Minimum Number of Groups.py
+++ 2406. Divide Intervals Into Minimum Number of Groups/2406. Divide Intervals Into Minimum Number of Groups.py
@@ -1,4 +1,26 @@
 from typing import List
 class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
+        start , end = [],[]
+
+        for left, right in intervals:
+            start.append(left)
+            end.append(right)
         
+        start.sort()
+        end.sort()
+
+        i,j=0,0
+        res = 0
+        groups = 0
+
+        while i< len(intervals):
+            if start[i]<=end[j]:
+                groups+=1
+                i+=1
+            else:
+                groups-=1
+                j+=1
+            
+            res = max(res,groups)
+        return res
