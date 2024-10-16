295e7a840837f662df5c263996d6a2a96d56e071
amineshx <amine.charef@etu.usthb.dz>
2024-10-16 23:24:59 +0100

    Update 2938. Separate Black and White Balls with not working sol

--- 2938. Separate Black and White Balls/2938. Separate Black and White Balls.py
+++ 2938. Separate Black and White Balls/2938. Separate Black and White Balls.py
@@ -1,3 +1,31 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
+        n = len(s)-1
+        i=n
+        res=0
+        while i >=0 :
+            print("i: ",i)
+            if s[i]=='1':
+                if i==n:
+                    i-=1
+                print("if s[i]=='1':")
+                j=i
+                print("j: ",j)
+                while j<n:
+                    temp=s[j]
+                    s[j]=s[j+1]
+                    s[j+1]=temp
+                    res+=1
+                    j+=1
+                    print(s)
+                    print("res", res)
+                    print("j: ",j)
+            else:
+                i-=1
+        return res
+
+sol = Solution()
+print(sol.minimumSteps("101"))
+
+
         
