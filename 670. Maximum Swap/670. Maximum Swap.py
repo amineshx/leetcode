aecd6c278ba0394f492865784f6ebf4fdb099cfa
amineshx <amine.charef@etu.usthb.dz>
2024-10-17 23:20:13 +0100

    Update 670. Maximum Swap

--- 670. Maximum Swap/670. Maximum Swap.py
+++ 670. Maximum Swap/670. Maximum Swap.py
@@ -1,3 +1,17 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
+        num = list(str(num))
+
+        max_digit = "0"
+        max_i = -1
+        swap_i = swap_j = -1
+        for i in reversed(range(len(num))):
+            if num[i]>max_digit:
+                max_digit =num[i]
+                max_i = i 
+            if num[i] < max_digit:
+                swap_i, swap_j = i, max_i
         
+        num[swap_i],num[swap_j]  = num[swap_j],num[swap_i]
+
+        return int("".join(num))
