ae75331e79031022e345005dcaf3fca9e2306e33
amineshx <amine.charef@etu.usthb.dz>
2024-10-22 00:14:21 +0100

    Update 1593. Split a String Into the Max Number of Unique Substrings

--- 1593. Split a String Into the Max Number of Unique Substrings/1593. Split a String Into the Max Number of Unique Substrings.py
+++ 1593. Split a String Into the Max Number of Unique Substrings/1593. Split a String Into the Max Number of Unique Substrings.py
@@ -1,3 +1,21 @@
 class Solution:
     def maxUniqueSplit(self, s: str) -> int:
+
+        def DFS(i, current_set):
+            if i==len(s):
+                return 0
         
+            res = 0
+            for j in range(i,len(s)):
+                substr = s[i:j+1]
+                if substr in current_set:
+                    continue
+
+                current_set.add(substr)
+                res = max(res,1+DFS(j+1, current_set))
+                current_set.remove(substr)
+            return res 
+        
+        return DFS(0,set())
+        
+    
