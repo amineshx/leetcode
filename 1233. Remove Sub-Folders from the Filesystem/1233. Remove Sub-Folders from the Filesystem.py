9232ee7fbd5891b879521d662d00fed9aef11180
amineshx <amine.charef@etu.usthb.dz>
2024-10-25 23:38:13 +0100

    Update 1233. Remove Sub-Folders from the Filesystem

--- 1233. Remove Sub-Folders from the Filesystem/1233. Remove Sub-Folders from the Filesystem.py
+++ 1233. Remove Sub-Folders from the Filesystem/1233. Remove Sub-Folders from the Filesystem.py
@@ -1,3 +1,10 @@
 from typing import List
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
+        folder.sort()
+        ans = [folder[0]]
+        for i in range(1, len(folder)):
+            last_folder = ans[-1] + '/'
+            if not folder[i].startswith(last_folder):
+                ans.append(folder[i])
+        return ans
