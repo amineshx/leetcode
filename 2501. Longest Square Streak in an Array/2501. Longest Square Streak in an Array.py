76e476ca8f2b9d204a1f0955ca7285578c528b8c
amineshx <amine.charef@etu.usthb.dz>
2024-10-29 00:19:41 +0100

    Update 2501. Longest Square Streak in an Array

--- 2501. Longest Square Streak in an Array/2501. Longest Square Streak in an Array.py
+++ 2501. Longest Square Streak in an Array/2501. Longest Square Streak in an Array.py
@@ -1,4 +1,18 @@
 from typing import List
 class Solution:
     def longestSquareStreak(self, nums: List[int]) -> int:
+        max_len = -1
+        num_set = set(nums)
+        sorted_nums = sorted(num_set)
+    
+        for num in sorted_nums:
+            count = 0
+            curr = num
+            while curr in num_set:
+                num_set.remove(curr)
+                curr = curr ** 2
+                count += 1
+            
+            max_len = max(max_len, count)
         
+        return max_len if max_len > 1 else -1
