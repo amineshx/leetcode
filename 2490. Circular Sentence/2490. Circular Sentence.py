1f30e1996abb3c76bd88afcb678e015a9cd49986
amineshx <amine.charef@etu.usthb.dz>
2024-11-02 23:40:30 +0100

    Update 2490. Circular Sentence

---
+++ 2490. Circular Sentence/2490. Circular Sentence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        i=0
+        while i <len(sentence)-1:
+            if sentence[i]==" " and i!= 0:
+                single_word=False
+                if sentence[i-1]!=sentence[i+1]:
+                    return False
+            i+=1
+
+        if sentence[0]!=sentence[-1]:
+            return False
+        return True
+
