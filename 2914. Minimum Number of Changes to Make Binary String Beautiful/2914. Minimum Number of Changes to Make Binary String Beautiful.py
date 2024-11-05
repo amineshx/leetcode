e143d88290063e692410107ad49d6bdd4689ca32
amineshx <amine.charef@etu.usthb.dz>
2024-11-05 23:50:43 +0100

    Update 2914. Minimum Number of Changes to Make Binary String Beautiful with solution one

--- 2914. Minimum Number of Changes to Make Binary String Beautiful/2914. Minimum Number of Changes to Make Binary String Beautiful.py
+++ 2914. Minimum Number of Changes to Make Binary String Beautiful/2914. Minimum Number of Changes to Make Binary String Beautiful.py
@@ -1,3 +1,11 @@
 class Solution:
     def minChanges(self, s: str) -> int:
-        
+        left = 0
+        res=0
+
+        for right in range(len(s)):
+            if s[left]!=s[right]:
+                if right%2==0:
+                    res+=1
+                left = right
+        return res 
