684cb5017e4926e8bc1bbc829a7a9f1870ad96fe
amineshx <amine.charef@etu.usthb.dz>
2024-11-09 00:49:46 +0100

    Update 1829. Maximum XOR for Each Query

--- 1829. Maximum XOR for Each Query/1829. Maximum XOR for Each Query.py
+++ 1829. Maximum XOR for Each Query/1829. Maximum XOR for Each Query.py
@@ -1,4 +1,14 @@
 from typing import List 
 class Solution:
     def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        res =0 
+        for num in nums:
+            res^=num
         
+        mask = (1<< maximumBit)-1
+        answer = []
+        for num in reversed(nums):
+            answer.append(res^mask)
+            res^=num
+        
+        return answer
