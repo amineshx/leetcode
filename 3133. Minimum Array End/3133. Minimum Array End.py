a9c4232eaa8884b45a2a6a47ace601b84cefc8be
amineshx <amine.charef@etu.usthb.dz>
2024-11-10 00:17:48 +0100

    Update 3133. Minimum Array End

--- 3133. Minimum Array End/3133. Minimum Array End.py
+++ 3133. Minimum Array End/3133. Minimum Array End.py
@@ -1,3 +1,16 @@
 class Solution:
     def minEnd(self, n: int, x: int) -> int:
-        
+        res=x
+        xi=1
+        ni=1
+
+        while ni <=n-1:
+            if xi & x ==0:
+                if ni & (n-1):
+                    res = res | xi
+                
+                ni = ni << 1
+            
+            xi = xi << 1
+
+        return res  
