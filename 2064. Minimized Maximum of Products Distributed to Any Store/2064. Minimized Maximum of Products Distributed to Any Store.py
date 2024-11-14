35d2fb71ffa3e2bad5d682d66394b71a6b75a765
amineshx <amine.charef@etu.usthb.dz>
2024-11-14 23:07:09 +0100

    Update 2064. Minimized Maximum of Products Distributed to Any Store

--- 2064. Minimized Maximum of Products Distributed to Any Store/2064. Minimized Maximum of Products Distributed to Any Store.py
+++ 2064. Minimized Maximum of Products Distributed to Any Store/2064. Minimized Maximum of Products Distributed to Any Store.py
@@ -1,4 +1,21 @@
 from typing import List
+from math import ceil
 class Solution:
     def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
+        def distribution(x):
+            stores = 0
+            for quantity in quantities:
+                stores+=ceil(quantity/x)
+            return stores<=n
         
+        left,right=1,max(quantities)
+        res = 0
+        while left<=right:
+            x = left + (right-left)//2
+            if distribution(x):
+                res = x
+                right = x-1
+            else:
+                left = x+1
+        
+        return res
