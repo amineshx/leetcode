ca9e33b1bd7b76ad94906ce7cbe6580a7e6fc4ec
amineshx <amine.charef@etu.usthb.dz>
2024-11-15 12:40:43 +0100

    Update 1574. Shortest Subarray to be Removed to Make Array Sorted

--- 1574. Shortest Subarray to be Removed to Make Array Sorted/1574. Shortest Subarray to be Removed to Make Array Sorted.py
+++ 1574. Shortest Subarray to be Removed to Make Array Sorted/1574. Shortest Subarray to be Removed to Make Array Sorted.py
@@ -1,4 +1,18 @@
 from typing import List
 class Solution:
     def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
-        
+        n = len(arr)
+        right = n-1
+        while right>0 and arr[right-1]<= arr[right]:
+            right -=1
+
+        res = right 
+        left = 0
+        while left<right:
+            while right<n and arr[left]>arr[right]:
+                right+=1
+            res = min(res, right-left-1)
+            if arr[left]> arr[left+1]:
+                break
+            left+=1
+        return res
