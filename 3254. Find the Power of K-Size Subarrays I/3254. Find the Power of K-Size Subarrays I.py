ac926e1af8db7f72d6a775fc6a59415fb1d3db3a
amineshx <amine.charef@etu.usthb.dz>
2024-11-16 12:37:35 +0100

    Update 3254. Find the Power of K-Size Subarrays I

--- 3254. Find the Power of K-Size Subarrays I/3254. Find the Power of K-Size Subarrays I.py
+++ 3254. Find the Power of K-Size Subarrays I/3254. Find the Power of K-Size Subarrays I.py
@@ -1,4 +1,21 @@
 from typing import List
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        left = 0
+        right = k-1
         
+        while right <len(nums):
+            check = True
+            for i in range(left,right):
+                if nums[i]>nums[i+1] or nums[i+1]-nums[i]!=1:
+                    res.append(-1)
+                    check=False
+                    break
+            if check:
+                res.append(nums[right])
+            right+=1
+            left+=1
+        return res
+
+
