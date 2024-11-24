cbf10f3d9b1bae4f88336635e7f0328d21d00518
amineshx <amine.charef@etu.usthb.dz>
2024-11-24 16:20:26 +0100

    Update 1975. Maximum Matrix Sum

--- 1975. Maximum Matrix Sum/1975. Maximum Matrix Sum.py
+++ 1975. Maximum Matrix Sum/1975. Maximum Matrix Sum.py
@@ -1,4 +1,17 @@
 from typing import List
 class Solution:
     def maxMatrixSum(self, matrix: List[List[int]]) -> int:
+        total_sum = 0
+        min_abs_value = float('inf')  
+        negative_count = 0
+        for row in matrix:
+            for value in row:
+                total_sum += abs(value)  
+                min_abs_value = min(min_abs_value, abs(value))  
+                if value < 0:
+                    negative_count += 1  
         
+        if negative_count % 2 == 1:
+            total_sum -= 2 * min_abs_value
+        
+        return total_sum
