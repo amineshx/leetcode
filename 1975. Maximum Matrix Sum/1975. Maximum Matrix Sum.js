65b6670849cf8733532b3d8a28124dfc45fb6df0
amineshx <amine.charef@etu.usthb.dz>
2024-11-24 16:45:00 +0100

    Update 1975. Maximum Matrix Sum.js

---
+++ 1975. Maximum Matrix Sum/1975. Maximum Matrix Sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var maxMatrixSum = function(matrix) {
+    let totalSum = 0
+    let min_abs = Infinity
+    let negative_nums = 0
+    for (row of matrix){
+        console.log(row)
+    }
+};
+
+maxMatrixSum(matrix = [[1,2,3],[-1,-2,-3],[1,2,3]])
