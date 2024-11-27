b7b686fe43bdfba5d50e09a6751de76eab8c3245
amineshx <amine.charef@etu.usthb.dz>
2024-11-27 23:26:42 +0100

    Update 3243. Shortest Distance After Road Addition Queries I.py

--- 3243. Shortest Distance After Road Addition Queries I/3243. Shortest Distance After Road Addition Queries I.py
+++ 3243. Shortest Distance After Road Addition Queries I/3243. Shortest Distance After Road Addition Queries I.py
@@ -1,4 +1,25 @@
 from typing import List
+from collections import deque
 class Solution:
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
-        
+        adjacency_list = [[i+1] for i in range(n)]
+
+        def BFS():
+            q = deque()
+            q.append((0,0))
+            visited = set()
+            visited.add((0,0))
+            while q:
+                current, length = q.popleft()
+                if current == n-1:
+                    return length
+                
+                for neighbore in adjacency_list[current]:
+                    if neighbore not in visited:
+                        q.append((neighbore,length+1))
+                        visited.add(neighbore)
+        res = []
+        for source, dist in queries:
+            adjacency_list[source].append(dist)
+            res.append(BFS())
+        return res
