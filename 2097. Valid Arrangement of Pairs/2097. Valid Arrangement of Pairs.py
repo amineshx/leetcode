6757aab61711ff9b893873ff377bcc15b35b2097
amineshx <amine.charef@etu.usthb.dz>
2024-11-30 23:06:53 +0100

    Update 2097. Valid Arrangement of Pairs.py

--- 2097. Valid Arrangement of Pairs/2097. Valid Arrangement of Pairs.py
+++ 2097. Valid Arrangement of Pairs/2097. Valid Arrangement of Pairs.py
@@ -1,4 +1,27 @@
 from typing import List
+from collections import defaultdict
 class Solution:
     def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
-        
+        adjacency_list = defaultdict(list)
+        inOutDeg = defaultdict(int)
+
+        for start, end in pairs:
+            adjacency_list[start].append(end)
+            inOutDeg[start] += 1  
+            inOutDeg[end] -= 1    
+
+        startNode = pairs[0][0] 
+        for node in inOutDeg:
+            if inOutDeg[node] == 1:
+                startNode = node
+                break
+
+        path = []
+        def dfs(curr):
+            while adjacency_list[curr]:
+                nextNode = adjacency_list[curr].pop()
+                dfs(nextNode)
+                path.append((curr, nextNode))
+
+        dfs(startNode)
+        return path[::-1]
