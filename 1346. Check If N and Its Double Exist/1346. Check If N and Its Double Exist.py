9e8e4f7e8ffe1c0e3eecbcffb3e770bfbc732166
amineshx <amine.charef@etu.usthb.dz>
2024-12-02 00:27:07 +0100

    Update 1346. Check If N and Its Double Exist.py

--- 1346. Check If N and Its Double Exist/1346. Check If N and Its Double Exist.py
+++ 1346. Check If N and Its Double Exist/1346. Check If N and Its Double Exist.py
@@ -1,3 +1,13 @@
+from typing import List
+
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        
+        seen = set()
+        for num in arr:
+            if 2*num in seen or num/2 in seen:
+                return True
+            seen.add(num)
+        return False
+
+sol = Solution()
+print(sol.checkIfExist(arr = [10,2,5,3]))
