20041e8310de235b952d51207925a7b7956f728e
amineshx <amine.charef@etu.usthb.dz>
2024-12-04 22:02:43 +0100

    Update 2825. Make String a Subsequence Using Cyclic Increments.py

--- 2825. Make String a Subsequence Using Cyclic Increments/2825. Make String a Subsequence Using Cyclic Increments.py
+++ 2825. Make String a Subsequence Using Cyclic Increments/2825. Make String a Subsequence Using Cyclic Increments.py
@@ -1,3 +1,16 @@
 class Solution:
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
-        
+        i=j=0
+        base=97
+        while j<len(str2) and i < len(str1):
+            c2 = ord(str2[j])-base
+            c1 = ord(str1[i])-base
+            if c1 == c2 or (c1+1)%26==c2:
+                j+=1
+            i+=1
+        return j==len(str2)
+                
+
+           
+
+
