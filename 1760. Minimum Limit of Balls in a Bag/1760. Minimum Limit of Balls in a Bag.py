9e6291590bf93181ff65b2efd7ba0cb2004db396
amineshx <amine.charef@etu.usthb.dz>
2024-12-07 20:32:18 +0100

    Update 1760. Minimum Limit of Balls in a Bag.py

--- 1760. Minimum Limit of Balls in a Bag/1760. Minimum Limit of Balls in a Bag.py
+++ 1760. Minimum Limit of Balls in a Bag/1760. Minimum Limit of Balls in a Bag.py
@@ -1,4 +1,22 @@
 from typing import List
+from math import ceil
 class Solution:
     def minimumSize(self, nums: List[int], maxOperations: int) -> int:
         
+        def isDividable(number):
+            operations = 0
+            for num in nums:
+                operations += ceil(num/number) -1
+                if operations > maxOperations:
+                    return False
+            return True
+        
+        left, right = 1, max(nums)
+        while left<right:
+            mid = left + ((right-left)//2)
+            if isDividable(mid):
+                right = mid
+            else:
+                left = mid+1
+        return left
+
