ae17ddbe9ad8995d28504ea85de4139b96c96017
amineshx <amine.charef@etu.usthb.dz>
2024-12-09 23:44:26 +0100

    Update 3152. Special Array II.py with a brut force approche

--- 3152. Special Array II/3152. Special Array II.py
+++ 3152. Special Array II/3152. Special Array II.py
@@ -1,4 +1,24 @@
 from typing import List
+
+# brut force approch
 class Solution:
     def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+        if len(nums)==1:
+                return True
         
+        def isIntervalSpecial(start, end):
+            
+            for i in range(start,end):
+                if nums[i]%2==0:
+                    if nums[i+1]%2!=1:
+                        return False
+                else:
+                    if nums[i+1]%2!=0:
+                        return False
+            return True
+        res = []
+        for querie in queries:
+            start,end = querie[0], querie[1]
+            res.append(isIntervalSpecial(start,end))
+        
+        return res
