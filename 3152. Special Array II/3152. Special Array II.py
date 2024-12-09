2382834783b7457465ce9023ae7e0fe9485a2dfa
amineshx <amine.charef@etu.usthb.dz>
2024-12-09 23:58:12 +0100

    Update 3152. Special Array II.py

--- 3152. Special Array II/3152. Special Array II.py
+++ 3152. Special Array II/3152. Special Array II.py
@@ -22,3 +22,20 @@ class Solution:
             res.append(isIntervalSpecial(start,end))
         
         return res
+
+
+class Solution:
+    def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+ 
+        n = len(nums)
+        segment_id = [0] * n
+        current_segment = 0
+        for i in range(1, n):
+            if nums[i] % 2 == nums[i - 1] % 2:
+                current_segment += 1
+            segment_id[i] = current_segment
+        
+        result = []
+        for start, end in queries:
+            result.append(segment_id[start] == segment_id[end])
+        return result
