a236681d41084011fb9621a991db1e7c903a8acf
amineshx <amine.charef@etu.usthb.dz>
2024-12-09 23:31:05 +0100

    Update 3151. Special Array I.py

--- 3151. Special Array I/3151. Special Array I.py
+++ 3151. Special Array I/3151. Special Array I.py
@@ -1,4 +1,14 @@
 from typing import List
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
-       
+        if len(nums)==1:
+            return True
+        
+        for i in range(len(nums)-1):
+            if nums[i]%2==0:
+                if nums[i+1]%2!=1:
+                    return False
+            else:
+                if nums[i+1]%2!=0:
+                    return False
+        return True
