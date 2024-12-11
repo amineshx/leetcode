2933b7ea5fc0b3cefba8e90d061e80883efde0d7
amineshx <amine.charef@etu.usthb.dz>
2024-12-12 00:25:02 +0100

    Update 2779. Maximum Beauty of an Array After Applying Operation.py

--- 2779. Maximum Beauty of an Array After Applying Operation/2779. Maximum Beauty of an Array After Applying Operation.py
+++ 2779. Maximum Beauty of an Array After Applying Operation/2779. Maximum Beauty of an Array After Applying Operation.py
@@ -1,4 +1,25 @@
 from typing import List
 class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
-        
+        n = len(nums)
+        max_value = 0
+
+        for num in nums:
+            if num > max_value:
+                max_value = num
+
+        range_array = [0] * (max_value + 10)
+
+        for num in nums:
+            left = max(0, num - k)
+            right = min(max_value, num + k) + 1
+            range_array[left] += 1
+            range_array[right] -= 1
+
+        result = range_array[0]
+        for i in range(1, len(range_array)):
+            range_array[i] += range_array[i - 1]
+            if range_array[i] > result:
+                result = range_array[i]
+
+        return result
