df2895d077ce278d335885506cf1f92566831f24
amineshx <amine.charef@etu.usthb.dz>
2024-12-14 23:55:22 +0100

    Update 2593. Find Score of an Array After Marking All Elements.py

--- 2593. Find Score of an Array After Marking All Elements/2593. Find Score of an Array After Marking All Elements.py
+++ 2593. Find Score of an Array After Marking All Elements/2593. Find Score of an Array After Marking All Elements.py
@@ -1,4 +1,16 @@
 from typing import List
 class Solution:
     def findScore(self, nums: List[int]) -> int:
-        
+        score=0
+        n = len(nums)
+        sorted_nums = sorted((num, idx) for idx, num in enumerate(nums))
+
+        for num, index in sorted_nums:
+            if nums[index]!=-1:
+                score+=num
+                nums[index]=-1
+                if index>0:
+                    nums[index-1]=-1
+                if index<n-1:
+                    nums[index+1]=-1
+        return score
