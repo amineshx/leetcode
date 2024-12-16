5ce264c78863e8d00bb36a16aa69cf2b9a585791
amineshx <amine.charef@etu.usthb.dz>
2024-12-17 00:36:40 +0100

    Update 3264. Final Array State After K Multiplication Operations I.py

--- 3264. Final Array State After K Multiplication Operations I/3264. Final Array State After K Multiplication Operations I.py
+++ 3264. Final Array State After K Multiplication Operations I/3264. Final Array State After K Multiplication Operations I.py
@@ -2,3 +2,12 @@ from typing import List
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
         
+        i=0
+        while i < k:
+            min_num = min(nums)
+            idx = nums.index(min_num)
+            nums[idx]=min_num*multiplier
+            i+=1
+        return nums
+
+
