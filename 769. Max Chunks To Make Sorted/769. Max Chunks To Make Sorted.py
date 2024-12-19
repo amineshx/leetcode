4ff7d4a26d4c2c1f600eb810a5a4b630adc7d5c7
amineshx <amine.charef@etu.usthb.dz>
2024-12-19 23:38:40 +0100

    Update 769. Max Chunks To Make Sorted.py

--- 769. Max Chunks To Make Sorted/769. Max Chunks To Make Sorted.py
+++ 769. Max Chunks To Make Sorted/769. Max Chunks To Make Sorted.py
@@ -1,4 +1,11 @@
 from typing import List
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
-        
+        current_max = -1
+        res=0
+        for i, num in enumerate(arr):
+            current_max=max(num, current_max)
+
+            if current_max==i:
+                res+=1
+        return res
