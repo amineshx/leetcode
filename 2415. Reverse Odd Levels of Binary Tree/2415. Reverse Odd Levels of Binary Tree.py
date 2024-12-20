96b44d4dfebc63ef6a1386076e122f9e013255e5
amineshx <amine.charef@etu.usthb.dz>
2024-12-20 17:39:58 +0100

    Update 2415. Reverse Odd Levels of Binary Tree.py

--- 2415. Reverse Odd Levels of Binary Tree/2415. Reverse Odd Levels of Binary Tree.py
+++ 2415. Reverse Odd Levels of Binary Tree/2415. Reverse Odd Levels of Binary Tree.py
@@ -1,10 +1,44 @@
 # Definition for a binary tree node.
-from typing import Optional
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+from typing import Optional
+from collections import deque
 class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        
+        q = deque([root])
+        i=0
+        while q:
+            if i % 2==1:
+                l,r=0,len(q)-1
+                while l<r:
+                    q[l].val,q[r].val=q[r].val,q[l].val
+                    l+=1
+                    r-=1
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            i+=1
+        return root
+
+
+# Helper function to construct a binary tree from a list
+def build_tree(values):
+    if not values:
+        return None
+    nodes = [TreeNode(val) if val is not None else None for val in values]
+    kids = nodes[::-1]
+    root = kids.pop()
+    for node in nodes:
+        if node:
+            if kids:
+                node.left = kids.pop()
+            if kids:
+                node.right = kids.pop()
+    return root
+
