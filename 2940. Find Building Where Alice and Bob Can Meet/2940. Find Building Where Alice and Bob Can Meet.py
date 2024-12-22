d2a06c60b9600d61269fcfa4ed640e843ed78ad2
amineshx <amine.charef@etu.usthb.dz>
2024-12-22 20:36:51 +0100

    Update 2940. Find Building Where Alice and Bob Can Meet.py

--- 2940. Find Building Where Alice and Bob Can Meet/2940. Find Building Where Alice and Bob Can Meet.py
+++ 2940. Find Building Where Alice and Bob Can Meet/2940. Find Building Where Alice and Bob Can Meet.py
@@ -1,4 +1,47 @@
 from typing import List
+
+'''
+
+this is an insane problem , and this is not my solution 
+
+'''
+
 class Solution:
     def leftmostBuildingQueries(self, heights: List[int], queries: List[List[int]]) -> List[int]:
-        
+        n = len(heights)
+        st = [[0] * 20 for _ in range(n)]
+        Log = [-1] * (n + 1)
+        Log[0] = -1
+        for i in range(1, n + 1):
+            Log[i] = Log[i >> 1] + 1
+        for i in range(n):
+            st[i][0] = heights[i]
+        for i in range(1, 20):
+            for j in range(n):
+                if j + (1 << i) <= n:
+                    st[j][i] = max(st[j][i - 1], st[j + (1 << (i - 1))][i - 1])
+
+        def Ask(l, r):
+            k = Log[r - l + 1]
+            return max(st[l][k], st[r - (1 << k) + 1][k])
+
+        res = []
+        for l, r in queries:
+            if l > r:
+                l, r = r, l
+            if l == r:
+                res.append(l)
+                continue
+            if heights[r] > heights[l]:
+                res.append(r)
+                continue
+            max_height = max(heights[r], heights[l])
+            left, right = r + 1, n
+            while left < right:
+                mid = (left + right) // 2
+                if Ask(r + 1, mid) > max_height:
+                    right = mid
+                else:
+                    left = mid + 1
+            res.append(left if left != n else -1)
+        return res
