f9e0a2d51417ec5cc608b2c984646021f2e5e8ea
amineshx <amine.charef@etu.usthb.dz>
2024-12-24 23:51:55 +0100

    Update 3203. Find Minimum Diameter After Merging Two Trees.py

--- 3203. Find Minimum Diameter After Merging Two Trees/3203. Find Minimum Diameter After Merging Two Trees.py
+++ 3203. Find Minimum Diameter After Merging Two Trees/3203. Find Minimum Diameter After Merging Two Trees.py
@@ -1,4 +1,35 @@
 from typing import List
+from collections import deque
 class Solution:
     def minimumDiameterAfterMerge(self, edges1: List[List[int]], edges2: List[List[int]]) -> int:
+        def diameter(edges):
+            n=len(edges)+1
+            deg=[0]*n
+            adj=[[] for _ in range(n)]
+            for v, w in edges:
+                adj[v].append(w)
+                adj[w].append(v)
+                deg[v]+=1
+                deg[w]+=1
+            q=deque()
+            for i, d in enumerate(deg):
+                if d==1:
+                    q.append(i)
+            level, left=0, n
+            while left>2:
+                qz=len(q)
+                left-=qz
+                for i in range(qz):
+                    v=q.popleft()
+                    for w in adj[v]:
+                        deg[w]-=1
+                        if deg[w]==1:
+                            q.append(w)
+                level+=1
+            return 2*level+1 if left==2 else 2*level
+
+        d1=diameter(edges1)
+        d2=diameter(edges2)
+        return max(d1, d2, (d1+1)//2+(d2+1)//2+1)
+        
         
