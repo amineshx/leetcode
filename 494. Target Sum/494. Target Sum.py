4e1600a3053673a54f7264e949b7a200564f9407
amineshx <amine.charef@etu.usthb.dz>
2024-12-26 16:21:50 +0100

    Update 494. Target Sum.py

--- 494. Target Sum/494. Target Sum.py
+++ 494. Target Sum/494. Target Sum.py
@@ -1,4 +1,17 @@
 from typing import List
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        memo = {}
+        def backtracking(i, current_sum):
+            if (i, current_sum) in memo:
+                return memo[(i,current_sum)]
+            if i==len(nums):
+                return 1 if current_sum == target else 0
+            
+            memo[(i,current_sum)] = (
+                backtracking(i+1, current_sum+nums[i])+
+                backtracking(i+1, current_sum-nums[i])
+            )
+            return memo[(i,current_sum)]
         
+        return backtracking(0,0)
