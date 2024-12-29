c3546e7c2c0eed5ed0313fff09ecb2254dfe77bd
amineshx <amine.charef@etu.usthb.dz>
2024-12-29 23:51:03 +0100

    Update 1639. Number of Ways to Form a Target String Given a Dictionary.py

--- 1639. Number of Ways to Form a Target String Given a Dictionary/1639. Number of Ways to Form a Target String Given a Dictionary.py
+++ 1639. Number of Ways to Form a Target String Given a Dictionary/1639. Number of Ways to Form a Target String Given a Dictionary.py
@@ -1,4 +1,29 @@
 from typing import List
+from collections import defaultdict
 class Solution:
     def numWays(self, words: List[str], target: str) -> int:
+        modulo = 10**9 + 7      # read description
+
+        count = defaultdict(int)
+        for word in words:
+            for i,w in enumerate(word):
+                count[(i,w)]+=1
         
+        memo = {}
+        def dfs(i,k):
+            if i == len(target):
+                return 1
+            if k == len(words[0]):
+                return 0 
+            if (i,k) in memo:
+                return memo[(i,k)]
+            
+            char = target[i]
+            memo[(i,k)] = dfs(i, k+1)
+            memo[(i,k)] += count[(k,char)]*dfs(i+1,k+1)
+            return memo[(i,k)] % modulo
+        
+        return dfs(0,0)
+
+sol =Solution()
+print(sol.numWays(words = ["acca","bbbb","caca"], target = "aba"))
