f6a031cb9105d974ed5cb9ec01f308e8b7246a92
amineshx <amine.charef@etu.usthb.dz>
2024-12-30 22:56:10 +0100

    Update 2466. Count Ways To Build Good Strings.py

--- 2466. Count Ways To Build Good Strings/2466. Count Ways To Build Good Strings.py
+++ 2466. Count Ways To Build Good Strings/2466. Count Ways To Build Good Strings.py
@@ -1,3 +1,19 @@
 class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        modulo = 10**9 + 7
+        memo = {}
+
+        def dfs(length):
+            if length > high:
+                return 0
+            
+            if length in memo:
+                return memo[length]
+            
+            memo[length] = 1 if length >= low else 0
+            memo[length] += dfs(length + zero) + dfs(length + one)
+
+            return memo[length] % modulo
         
+        return dfs(0)
+            
