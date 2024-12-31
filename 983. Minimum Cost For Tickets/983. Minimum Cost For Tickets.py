e72fb2b7324109788b2b6607f1898219d143eca2
amineshx <amine.charef@etu.usthb.dz>
2025-01-01 00:03:38 +0100

    Update 983. Minimum Cost For Tickets.py

--- 983. Minimum Cost For Tickets/983. Minimum Cost For Tickets.py
+++ 983. Minimum Cost For Tickets/983. Minimum Cost For Tickets.py
@@ -11,3 +11,6 @@ class Solution:
                     j+=1
                 dp[i] = min(dp[i], cost+dp[j])
         return dp[0]
+
+sol = Solution()
+print(sol.mincostTickets(days = [1,4,6,7,8,20], costs = [2,7,15]))
