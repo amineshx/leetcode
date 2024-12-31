991d5c7a4f88cf96563a4053bac47a002993be89
amineshx <amine.charef@etu.usthb.dz>
2025-01-01 00:03:07 +0100

    Update 983. Minimum Cost For Tickets.py

--- 983. Minimum Cost For Tickets/983. Minimum Cost For Tickets.py
+++ 983. Minimum Cost For Tickets/983. Minimum Cost For Tickets.py
@@ -1,4 +1,13 @@
 from typing import List
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        
+        dp = [0] * (len(days)+1)
+
+        for i in reversed(range(len(days))):
+            j=i
+            dp[i]=float("inf")
+            for cost, duariation in zip(costs,[1,7,30]):
+                while j< len(days) and days[j]< days[i]+duariation:
+                    j+=1
+                dp[i] = min(dp[i], cost+dp[j])
+        return dp[0]
