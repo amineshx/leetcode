c67491d007c3eb8382edc178c91566826848b90f
amineshx <amine.charef@etu.usthb.dz>
2025-01-02 21:06:02 +0100

    Update 2559. Count Vowel Strings in Ranges.py

--- 2559. Count Vowel Strings in Ranges/2559. Count Vowel Strings in Ranges.py
+++ 2559. Count Vowel Strings in Ranges/2559. Count Vowel Strings in Ranges.py
@@ -1,4 +1,18 @@
 from typing import List
 class Solution:
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
+        vowls = set("aeiou")
+        prefix = [0]*(len(words)+1)
+        prev = 0
+
+        for i, word in enumerate(words):
+            if word[0] in vowls and word[-1] in vowls:
+                prev+=1
+            prefix[i+1] = prev
         
+        res = [0]*len(queries)
+
+        for i, querie in enumerate(queries):
+            left, right = querie
+            res[i] = prefix[right+1]-prefix[left]
+        return res
