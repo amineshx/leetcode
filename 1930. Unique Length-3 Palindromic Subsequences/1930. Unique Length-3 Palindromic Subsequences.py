c92e7ca79a64f1c9ac283842233429c074eac30b
amineshx <amine.charef@etu.usthb.dz>
2025-01-04 16:52:30 +0100

    Update 1930. Unique Length-3 Palindromic Subsequences.py

--- 1930. Unique Length-3 Palindromic Subsequences/1930. Unique Length-3 Palindromic Subsequences.py
+++ 1930. Unique Length-3 Palindromic Subsequences/1930. Unique Length-3 Palindromic Subsequences.py
@@ -1,3 +1,18 @@
+from collections import Counter
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
+        res = set()
+        left_side = set()
+        right_side = Counter(s)
+
+        for mid in s:
+            right_side[mid]-=1
+            for char in left_side:
+                if right_side[char]>0:
+                    res.add((mid, char))
+            left_side.add(mid)
         
+        return len(res)
+
+sol = Solution()
+print(sol.countPalindromicSubsequence(s = "aabca"))
