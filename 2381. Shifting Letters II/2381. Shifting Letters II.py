92db4de1a2879ebb4206271090c16d2c181ac5f6
amineshx <amine.charef@etu.usthb.dz>
2025-01-06 00:50:55 +0100

    Update 2381. Shifting Letters II.py

--- 2381. Shifting Letters II/2381. Shifting Letters II.py
+++ 2381. Shifting Letters II/2381. Shifting Letters II.py
@@ -1,2 +1,19 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: list[list[int]]) -> str:
+        n = len(s)
+        shift = [0] * (n + 1)
+
+        for shiftOp in shifts:
+            start, end, direction = shiftOp
+            shift[start] += (1 if direction == 1 else -1)
+            if end + 1 < n:
+                shift[end + 1] -= (1 if direction == 1 else -1)
+
+        currentShift = 0
+        shiftList = list(s)
+        for i in range(n):
+            currentShift += shift[i]
+            netShift = (currentShift % 26 + 26) % 26
+            shiftList[i] = chr((ord(shiftList[i]) - ord('a') + netShift) % 26 + ord('a'))
+
+        return ''.join(shiftList)
