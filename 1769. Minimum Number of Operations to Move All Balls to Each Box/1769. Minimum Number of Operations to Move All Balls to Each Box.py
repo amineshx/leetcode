cd0ec60ded1a1c72756bfacebe5a79a97cb84e4b
amineshx <amine.charef@etu.usthb.dz>
2025-01-06 19:15:55 +0100

    Update 1769. Minimum Number of Operations to Move All Balls to Each Box.py

--- 1769. Minimum Number of Operations to Move All Balls to Each Box/1769. Minimum Number of Operations to Move All Balls to Each Box.py
+++ 1769. Minimum Number of Operations to Move All Balls to Each Box/1769. Minimum Number of Operations to Move All Balls to Each Box.py
@@ -1,4 +1,19 @@
 from typing import List
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
+        res = [0]*len(boxes)
+
+        balles, moves = 0,0
+
+        for i in range(len(boxes)):
+            res[i]+=balles+moves
+            moves+=balles
+            balles+=int(boxes[i])
         
+        balles, moves = 0,0
+        for i in reversed(range(len(boxes))):
+            res[i]+=balles+moves
+            moves+=balles
+            balles+=int(boxes[i])
+        
+        return res
