cd1f6f33ff3bc0fcb77d2506a855c4ce8356e96a
amineshx <amine.charef@etu.usthb.dz>
2025-01-07 23:11:51 +0100

    Update 1408. String Matching in an Array.py

--- 1408. String Matching in an Array/1408. String Matching in an Array.py
+++ 1408. String Matching in an Array/1408. String Matching in an Array.py
@@ -1,3 +1,13 @@
 from typing import List
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
+        n = len(words)
+        ans = []
+
+        for i in range(n):
+            for j in range(n):
+                if i != j and words[j].find(words[i]) != -1:
+                    ans.append(words[i])
+                    break
+
+        return ans
