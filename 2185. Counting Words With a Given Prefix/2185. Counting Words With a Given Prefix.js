55416cd1e390aafe20b0584cd929e9736533299a
amineshx <amine.charef@etu.usthb.dz>
2025-01-09 23:02:16 +0100

    Update 2185. Counting Words With a Given Prefix.js

--- 2185. Counting Words With a Given Prefix/2185. Counting Words With a Given Prefix.js
+++ 2185. Counting Words With a Given Prefix/2185. Counting Words With a Given Prefix.js
@@ -4,5 +4,11 @@
  * @return {number}
  */
 var prefixCount = function(words, pref) {
-    
+  let res = 0
+  for(let word of words){
+    if (word.startsWith(pref)){
+        res++
+    }
+  }
+  return res  
 };
