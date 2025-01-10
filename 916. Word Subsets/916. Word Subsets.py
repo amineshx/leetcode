349edddaf4ff90aa6d9548faa39bdf9f95ab4d0e
amineshx <amine.charef@etu.usthb.dz>
2025-01-10 18:05:16 +0100

    Update 916. Word Subsets.py

--- 916. Word Subsets/916. Word Subsets.py
+++ 916. Word Subsets/916. Word Subsets.py
@@ -1,9 +1,27 @@
 from typing import List
+from collections import defaultdict, Counter
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        res = []
+        count_words2 = defaultdict(int)
+        for word in words2:
+            count_word = Counter(word)   
+            for c, count in count_word.items():
+                count_words2[c] = max(count,count_words2[c])     
         
-        return [word for word in words1 if all(c in word for c in words2)]
+        for word in words1:
+            count_word = Counter(word)
+            check = True
+            for char, count in count_words2.items():
+
+                if count_word[char] < count:
+                    check = False
+                    break
+            if check:
+                res.append(word)
+        
+        return res
         
 
 sol = Solution()
-print(sol.wordSubsets(words1 = ["amazon","apple","facebook","google","leetcode"], words2 = ["e","o"]))
+print(sol.wordSubsets(words1 = ["amazon","apple","facebook","google","leetcode"], words2 = ["e","o","e"]))
