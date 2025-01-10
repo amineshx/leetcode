bbe6d618c4e766ec143aa78b2a9031715fc53f26
amineshx <amine.charef@etu.usthb.dz>
2025-01-10 17:35:46 +0100

    Update 916. Word Subsets.py this solution doest pass all test cases

--- 916. Word Subsets/916. Word Subsets.py
+++ 916. Word Subsets/916. Word Subsets.py
@@ -2,3 +2,8 @@ from typing import List
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
         
+        return [word for word in words1 if all(c in word for c in words2)]
+        
+
+sol = Solution()
+print(sol.wordSubsets(words1 = ["amazon","apple","facebook","google","leetcode"], words2 = ["e","o"]))
