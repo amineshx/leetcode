383ce610b9841e504c1a0b9d478325c242eb30d2
amineshx <amine.charef@etu.usthb.dz>
2025-01-11 17:06:17 +0100

    Update 1400. Construct K Palindrome Strings.py

--- 1400. Construct K Palindrome Strings/1400. Construct K Palindrome Strings.py
+++ 1400. Construct K Palindrome Strings/1400. Construct K Palindrome Strings.py
@@ -1,3 +1,12 @@
+from collections import Counter
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
+        if k > len(s):
+            return False
         
+        count  = Counter(s)
+        odd=0
+        for cnt in count.values():
+            odd += cnt%2
+        
+        return odd <=k
