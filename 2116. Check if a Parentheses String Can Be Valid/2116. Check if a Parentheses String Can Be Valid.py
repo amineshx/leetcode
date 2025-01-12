952d01c420c6a16c175fc86208e4036e56cc6ad3
amineshx <amine.charef@etu.usthb.dz>
2025-01-12 17:34:29 +0100

    Update 2116. Check if a Parentheses String Can Be Valid.py

--- 2116. Check if a Parentheses String Can Be Valid/2116. Check if a Parentheses String Can Be Valid.py
+++ 2116. Check if a Parentheses String Can Be Valid/2116. Check if a Parentheses String Can Be Valid.py
@@ -1,3 +1,39 @@
 class Solution:
     def canBeValid(self, s: str, locked: str) -> bool:
         
+        if len(s)%2 != 0:
+            return False
+        
+        locked_count, unlocked_count = 0, 0
+        for i in range(len(s)):
+            if locked[i] == "0":
+                unlocked_count+=1
+            elif s[i] =="(":
+                locked_count+=1
+            else:
+                if locked_count:
+                    locked_count -=1
+                elif unlocked_count:
+                    unlocked_count-=1
+                else:
+                    return False
+
+        if locked_count == 0:
+            return True
+        
+        locked_count, unlocked_count = 0, 0
+
+        for i in reversed(range(len(s))):
+            if locked[i]=="0":
+                unlocked_count+=1
+            elif s[i] ==")":
+                locked_count+=1
+            else:
+                if locked_count:
+                    locked_count -=1
+                elif unlocked_count:
+                    unlocked_count-=1
+                else:
+                    return False
+        
+        return True
