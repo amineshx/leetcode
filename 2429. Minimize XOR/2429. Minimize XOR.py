8d8e4627f7debc2f0a43c803f3648c4fbcba9e8c
amineshx <amine.charef@etu.usthb.dz>
2025-01-15 09:45:13 +0100

    Update 2429. Minimize XOR.py

--- 2429. Minimize XOR/2429. Minimize XOR.py
+++ 2429. Minimize XOR/2429. Minimize XOR.py
@@ -1,2 +1,12 @@
 class Solution:
     def minimizeXor(self, num1: int, num2: int) -> int:
+        x, y = bin(num1).count('1'), bin(num2).count('1')
+        res = num1
+        for i in range(32):
+            if x > y and (1 << i) & num1 > 0:
+                res ^= 1 << i
+                x -= 1
+            if x < y and (1 << i) & num1 == 0:
+                res ^= 1 << i
+                x += 1
+        return res
