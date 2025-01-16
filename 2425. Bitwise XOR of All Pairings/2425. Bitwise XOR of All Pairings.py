c4277fbd176debc8176b21a1b9285dd7548023b7
amineshx <amine.charef@etu.usthb.dz>
2025-01-16 23:20:43 +0100

    Update 2425. Bitwise XOR of All Pairings.py

--- 2425. Bitwise XOR of All Pairings/2425. Bitwise XOR of All Pairings.py
+++ 2425. Bitwise XOR of All Pairings/2425. Bitwise XOR of All Pairings.py
@@ -1,4 +1,14 @@
 from typing import List
 class Solution:
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
-        
+        n = len(nums1)
+        m = len(nums2)
+        x = 0
+        y = 0
+        if n % 2 != 0:
+            for i in nums2:
+                y ^= i
+        if m % 2 != 0:
+            for i in nums1:
+                x ^= i
+        return x ^ y
