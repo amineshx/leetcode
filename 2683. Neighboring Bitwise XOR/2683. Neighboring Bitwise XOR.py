f6bf4b3dba04f8291a5bb1a0ee8c45888e1c177a
amineshx <amine.charef@etu.usthb.dz>
2025-01-17 19:21:11 +0100

    Update 2683. Neighboring Bitwise XOR.py

--- 2683. Neighboring Bitwise XOR/2683. Neighboring Bitwise XOR.py
+++ 2683. Neighboring Bitwise XOR/2683. Neighboring Bitwise XOR.py
@@ -1,4 +1,9 @@
 from typing import List
 class Solution:
     def doesValidArrayExist(self, derived: List[int]) -> bool:
+        x=0
+        for n in derived:
+            if n:
+                x=~x
         
+        return 0==x
