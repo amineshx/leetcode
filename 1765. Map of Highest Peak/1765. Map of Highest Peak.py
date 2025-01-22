15e021555522a20d15be46b561a55631603c851e
amineshx <amine.charef@etu.usthb.dz>
2025-01-22 16:14:27 +0100

    Update 1765. Map of Highest Peak.py

--- 1765. Map of Highest Peak/1765. Map of Highest Peak.py
+++ 1765. Map of Highest Peak/1765. Map of Highest Peak.py
@@ -4,4 +4,24 @@ class Solution:
     def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
         n,m = len(isWater), len(isWater[0])
         q= deque()
-        visited = set()
+        res = [[-1]*m for _ in range(n)]
+
+        for row in range(n):
+            for col in range(m):
+                if isWater[row][col]==1:
+                    q.append((row,col))
+                    res[row][col]=0
+
+        while q:
+            row, col = q.popleft()
+            hight = res[row][col]
+
+            neighbores = [[row+1,col],[row,col+1],[row-1,col],[row,col-1]]
+            for rowNei, colNei in neighbores:
+                if (rowNei<0 or colNei<0 or rowNei==n or colNei==m or res[rowNei][colNei]!=-1):
+                    continue
+                q.append((rowNei,colNei))
+                res[rowNei][colNei]=hight+1
+        
+        return res
+
