b853d39b5d7c4ca87ffda8ad370c64a00c06c0ad
amineshx <amine.charef@etu.usthb.dz>
2025-01-22 16:17:28 +0100

    Update 1765. Map of Highest Peak.js

---
+++ 1765. Map of Highest Peak/1765. Map of Highest Peak.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[][]} isWater
+ * @return {number[][]}
+ */
+var highestPeak = function(isWater) {
+    const n = isWater.length
+    const m = isWater[0].length
+    
+};
+highestPeak(isWater = [[0,1],[0,0]])
