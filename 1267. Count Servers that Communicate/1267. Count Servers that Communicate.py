374e67558f47e172dd8fa1238cdb84fba08d759a
amineshx <amine.charef@etu.usthb.dz>
2025-01-23 19:46:50 +0100

    Update 1267. Count Servers that Communicate.py with a brutforce solution + optimezed space complexity

--- 1267. Count Servers that Communicate/1267. Count Servers that Communicate.py
+++ 1267. Count Servers that Communicate/1267. Count Servers that Communicate.py
@@ -1,4 +1,5 @@
 from typing import List
+# bruteforce solution
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
         m,n = len(grid), len(grid[0])
@@ -17,4 +18,31 @@ class Solution:
             for col in range(n):
                 if grid[row][col] and max(row_count[row],col_count[col]) > 1:
                     res+=1
+        return res
+
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+        m,n = len(grid), len(grid[0])
+        res = 0
+        for row in range(m):
+            row_sum = sum(grid[row])
+            if row_sum<=1:
+                continue
+            res+=row_sum
+
+            for col in range(n):
+                if grid[row][col]:
+                    grid[row][col]=-1
+
+        for col in range(n):
+            col_sum = 0
+            unmarcked = 0
+            for row in range(m):
+                col_sum+=abs(grid[row][col])
+                if grid[row][col]>0:
+                    unmarcked+=1
+                elif grid[row][col]<0:
+                    grid[row][col]=1
+            if col_sum>=2:
+                res+=unmarcked
         return res
