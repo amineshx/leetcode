3f8388ecdf6326b1c6b0fa76edf4f8813a241d2d
amineshx <amine.charef@etu.usthb.dz>
2025-01-23 19:36:23 +0100

    Update 1267. Count Servers that Communicate.py with a brutforce solution

--- 1267. Count Servers that Communicate/1267. Count Servers that Communicate.py
+++ 1267. Count Servers that Communicate/1267. Count Servers that Communicate.py
@@ -1,4 +1,20 @@
 from typing import List
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
+        m,n = len(grid), len(grid[0])
+
+        row_count = [0]*m
+        col_count = [0]*n 
+
+        for row in range(m):
+            for col in range(n):
+                if grid[row][col]==1:
+                    row_count[row]+=1
+                    col_count[col]+=1
         
+        res = 0
+        for row in range(m):
+            for col in range(n):
+                if grid[row][col] and max(row_count[row],col_count[col]) > 1:
+                    res+=1
+        return res
