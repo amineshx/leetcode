c7d1db65a08c93318fff49f5d26c41cdec1a97d7
amineshx <amine.charef@etu.usthb.dz>
2025-01-24 22:04:00 +0100

    Update 802. Find Eventual Safe States.py

--- 802. Find Eventual Safe States/802. Find Eventual Safe States.py
+++ 802. Find Eventual Safe States/802. Find Eventual Safe States.py
@@ -1,4 +1,5 @@
 from typing import List
+# sol1 dosn't pass all test cases
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         
@@ -9,12 +10,40 @@ class Solution:
         res = terminal_node
         for node, node_val in enumerate(graph):
             for terminal in terminal_node:
-                if terminal in node_val:
+                if terminal in node_val and len(node_val)==1:
                     
                     res.append(node)
         res.sort()
         return res
 
+
+
+# sol 2 
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        n = len(graph)
+        color = [0] * n  
+        
+        def dfs(node: int) -> bool:
+            if color[node] > 0:
+                return color[node] == 2
+            
+            color[node] = 1  
+            for neighbor in graph[node]:
+                if color[neighbor] == 2:
+                    continue
+                if color[neighbor] == 1 or not dfs(neighbor):
+                    return False
+            
+            color[node] = 2  
+            return True
+        
+        res = []
+        for i in range(n):
+            if dfs(i):
+                res.append(i)
+        
+        return res
 
 sol = Solution()
 print(sol.eventualSafeNodes(graph = [[1,2],[2,3],[5],[0],[5],[],[]]))
