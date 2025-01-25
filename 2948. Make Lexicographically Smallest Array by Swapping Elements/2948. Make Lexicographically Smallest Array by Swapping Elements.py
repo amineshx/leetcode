3a578e6e67fe916754f58e3fc860f1245294af56
amineshx <amine.charef@etu.usthb.dz>
2025-01-25 19:50:11 +0100

    Update 2948. Make Lexicographically Smallest Array by Swapping Elements.py

--- 2948. Make Lexicographically Smallest Array by Swapping Elements/2948. Make Lexicographically Smallest Array by Swapping Elements.py
+++ 2948. Make Lexicographically Smallest Array by Swapping Elements/2948. Make Lexicographically Smallest Array by Swapping Elements.py
@@ -1,4 +1,18 @@
 from typing import List
+from collections import deque
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
+        groups = []
+        num_to_group = {}
+
+        for num in sorted(nums):
+            if not groups or abs(num- groups[-1][-1])>limit:
+                groups.append(deque())
+            groups[-1].append(num)
+            num_to_group[num]=len(groups)-1
         
+        res = []
+        for num in nums:
+            j=num_to_group[num]
+            res.append(groups[j].popleft())
+        return res
