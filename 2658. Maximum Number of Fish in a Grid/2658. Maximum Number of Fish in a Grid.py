4a06f08451675cbfcf0812de11a49a3e9267130e
amineshx <amine.charef@etu.usthb.dz>
2025-01-28 21:17:35 +0100

    Update 2658. Maximum Number of Fish in a Grid.py

--- 2658. Maximum Number of Fish in a Grid/2658. Maximum Number of Fish in a Grid.py
+++ 2658. Maximum Number of Fish in a Grid/2658. Maximum Number of Fish in a Grid.py
@@ -2,7 +2,7 @@ from typing import List
 class Solution:
     def findMaxFish(self, grid: List[List[int]]) -> int:
         def DFS(row,col):
-            if (row<0 or row==n or col<0 or row==m or visited[row][col]==1) or grid[row][col]==0:
+            if (row<0 or row==n or col<0 or col==m or visited[row][col]==1) or grid[row][col]==0:
                 return 0
             visited[row][col]=1
             diractions = [[1,0],[-1,0],[0,1],[0,-1]]
@@ -20,3 +20,7 @@ class Solution:
                 if grid[row][col] or visited[row][col]==-1:
                     res=max(DFS(row,col),res)
         return res
+
+sol = Solution()
+print(sol.findMaxFish(grid = [[0,2,1,0],[4,0,0,3],[1,0,0,4],[0,3,2,0]]))
+print(sol.findMaxFish(grid = [[1,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,1]]))
