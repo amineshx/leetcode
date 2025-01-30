8a378d389a0a41062a60ff64398e83fdb6ef9b43
amineshx <amine.charef@etu.usthb.dz>
2025-01-30 22:19:34 +0100

    Update 2493. Divide Nodes Into the Maximum Number of Groups.py

--- 2493. Divide Nodes Into the Maximum Number of Groups/2493. Divide Nodes Into the Maximum Number of Groups.py
+++ 2493. Divide Nodes Into the Maximum Number of Groups/2493. Divide Nodes Into the Maximum Number of Groups.py
@@ -1,4 +1,26 @@
 from typing import List
+from collections import defaultdict, deque
 class Solution:
     def magnificentSets(self, n: int, edges: List[List[int]]) -> int:
-        
+        g = [[] for _ in range(n)]
+        for a, b in edges:
+            g[a - 1].append(b - 1)
+            g[b - 1].append(a - 1)
+        d = defaultdict(int)
+        for i in range(n):
+            q = deque([i])
+            dist = [0] * n
+            dist[i] = mx = 1
+            root = i
+            while q:
+                a = q.popleft()
+                root = min(root, a)
+                for b in g[a]:
+                    if dist[b] == 0:
+                        dist[b] = dist[a] + 1
+                        mx = max(mx, dist[b])
+                        q.append(b)
+                    elif abs(dist[b] - dist[a]) != 1:
+                        return -1
+            d[root] = max(d[root], mx)
+        return sum(d.values())
