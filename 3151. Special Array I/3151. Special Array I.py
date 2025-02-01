91a22e8b40b2174b186437291f938a44c8f69855
amineshx <amine.charef@etu.usthb.dz>
2025-02-01 15:01:03 +0100

    Update 3151. Special Array I.py

--- 3151. Special Array I/3151. Special Array I.py
+++ 3151. Special Array I/3151. Special Array I.py
@@ -13,5 +13,11 @@ class Solution:
                     return False
         return True
 
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        for i in range(1,len(nums)):
+            if nums[i] & 1 == nums[i-1]&1:
+                return False
+        return True
 sol = Solution()
 print(sol.isArraySpecial(nums = [2,1,4]))
