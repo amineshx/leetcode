e4e281decaf16403a0b5afee06da072a0d65f4e1
amineshx <amine.charef@etu.usthb.dz>
2025-02-01 15:05:15 +0100

    Update 3151. Special Array I.js

--- 3151. Special Array I/3151. Special Array I.js
+++ 3151. Special Array I/3151. Special Array I.js
@@ -3,5 +3,10 @@
  * @return {boolean}
  */
 var isArraySpecial = function(nums) {
-    
+  for (let i=1 ; i<nums.length; i++){
+    if ((nums[i]&1) === (nums[i-1]&1)){
+        return false
+        }
+    }
+    return true
 };
