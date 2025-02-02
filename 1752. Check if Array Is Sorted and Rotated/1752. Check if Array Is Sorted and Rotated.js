d35fd3364b0ac5e30ff137d8066c1a0d11b886a7
amineshx <amine.charef@etu.usthb.dz>
2025-02-02 23:34:29 +0100

    Update 1752. Check if Array Is Sorted and Rotated.js

--- 1752. Check if Array Is Sorted and Rotated/1752. Check if Array Is Sorted and Rotated.js
+++ 1752. Check if Array Is Sorted and Rotated/1752. Check if Array Is Sorted and Rotated.js
@@ -3,4 +3,18 @@
  * @return {boolean}
  */
 var check = function(nums) {
+    n = nums.length
+    window_length = 1
+    for (let i=1; i<2*n; i++){
+        if (nums[(i-1)%n]<= nums[i%n]){
+            window_length++
+        }else{
+            window_length=1
+        }
+        
+        if (n===window_length){
+            return true
+        }
+    }
+    return n==1
 };
