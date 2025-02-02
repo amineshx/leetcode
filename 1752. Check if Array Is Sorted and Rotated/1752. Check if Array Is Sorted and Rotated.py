9760f342d541154fc44f61e7f64205a3b7f6f4b0
amineshx <amine.charef@etu.usthb.dz>
2025-02-02 23:29:43 +0100

    Update 1752. Check if Array Is Sorted and Rotated.py

--- 1752. Check if Array Is Sorted and Rotated/1752. Check if Array Is Sorted and Rotated.py
+++ 1752. Check if Array Is Sorted and Rotated/1752. Check if Array Is Sorted and Rotated.py
@@ -1,4 +1,15 @@
 from typing import List
 class Solution:
     def check(self, nums: List[int]) -> bool:
+        n = len(nums)
+        window_length = 1
+
+        for i in range(1,2*n):
+            if nums[(i-1)%n] <=nums[i%n]:
+                window_length+=1
+
+            else: window_length=1
+
+            if window_length==n:return True
         
+        return n==1
