082cbccbd34819e85c94d976c8c344759ccdd658
amineshx <amine.charef@etu.usthb.dz>
2025-02-03 20:47:33 +0100

    Update 3105. Longest Strictly Increasing or Strictly Decreasing Subarray.py sol2

--- 3105. Longest Strictly Increasing or Strictly Decreasing Subarray/3105. Longest Strictly Increasing or Strictly Decreasing Subarray.py
+++ 3105. Longest Strictly Increasing or Strictly Decreasing Subarray/3105. Longest Strictly Increasing or Strictly Decreasing Subarray.py
@@ -1,4 +1,5 @@
 from typing import List
+# sol 1 
 class Solution:
     def longestMonotonicSubarray(self, nums: List[int]) -> int:
         n = len(nums)
@@ -17,4 +18,30 @@ class Solution:
             else:
                 window_length= 1
             res = max(res,window_length)
+        return res
+
+# sol2 
+class Solution:
+    def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        n=len(nums)
+        window_lenght = 1
+        res = 1
+        increasing = 0
+        for i in range(1,n):
+            if nums[i-1]<nums[i]:
+                if increasing>0:
+                    window_lenght+=1
+                else:
+                    window_lenght=2
+                    increasing=1
+            elif nums[i-1]>nums[i]:
+                if increasing < 0:
+                    window_lenght+=1
+                else:
+                    window_lenght=2
+                    increasing=-1
+            else:
+                window_lenght=1
+                increasing=0
+            res = max(res,window_lenght)
         return res
