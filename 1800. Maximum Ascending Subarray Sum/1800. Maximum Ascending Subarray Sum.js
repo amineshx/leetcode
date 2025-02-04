3bbc84186cdc2a073470990db4ed45185c491ce5
amineshx <amine.charef@etu.usthb.dz>
2025-02-04 21:21:21 +0100

    Update 1800. Maximum Ascending Subarray Sum.js

--- 1800. Maximum Ascending Subarray Sum/1800. Maximum Ascending Subarray Sum.js
+++ 1800. Maximum Ascending Subarray Sum/1800. Maximum Ascending Subarray Sum.js
@@ -4,4 +4,20 @@
  */
 var maxAscendingSum = function(nums) {
     
+    const n = nums.length
+    let max_sum = nums[0]
+    let res = max_sum
+    
+    for (let i=1; i<n; i++){
+        if (nums[i-1]<nums[i]){
+            max_sum+=nums[i]
+        }
+        else{
+            res = Math.max(res,max_sum)
+            max_sum=nums[i]
+        }
+    }
+    res = Math.max(res,max_sum)
+    return res
+
 };
