bcaba15c0dff5c5d8f7e87eef37ecef10d48fe17
amineshx <amine.charef@etu.usthb.dz>
2025-02-04 21:13:43 +0100

    Update 1800. Maximum Ascending Subarray Sum.py

--- 1800. Maximum Ascending Subarray Sum/1800. Maximum Ascending Subarray Sum.py
+++ 1800. Maximum Ascending Subarray Sum/1800. Maximum Ascending Subarray Sum.py
@@ -1,4 +1,17 @@
 from typing import List
 class Solution:
     def maxAscendingSum(self, nums: List[int]) -> int:
-        
+        n=len(nums)
+        max_sum = nums[0]
+        current_max = 0
+        for i in range(1,n):
+            if nums[i-1]<nums[i]:
+                max_sum+=nums[i]
+            else:
+                current_max=max(current_max,max_sum)
+                max_sum=nums[i]
+        current_max=max(current_max,max_sum)
+        return current_max  
+
+sol = Solution()
+print(sol.maxAscendingSum(nums = [12,17,15,13,10,11,12]))
