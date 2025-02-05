e39c115f22207d2728d82ca801f97fca2796d7fd
amineshx <amine.charef@etu.usthb.dz>
2025-02-05 15:42:39 +0100

    Update 1790. Check if One String Swap Can Make Strings Equal.js

--- 1790. Check if One String Swap Can Make Strings Equal/1790. Check if One String Swap Can Make Strings Equal.js
+++ 1790. Check if One String Swap Can Make Strings Equal/1790. Check if One String Swap Can Make Strings Equal.js
@@ -3,23 +3,29 @@
  * @param {string} s2
  * @return {boolean}
  */
+const getFrequencyMap = (str) => {
+    const map = new Map();
+    for (const char of str) {
+        map.set(char, (map.get(char) || 0) + 1)
+    }
+    return map;
+}
 var areAlmostEqual = function(s1, s2) {
     const n = s1.length
     let count_diff = 0
-    const haveSameChars = (s1,s2) =>{
-        const charCount = (str) =>
-            str.split("").reduce((acc, char)=>{
-                acc[char]=(acc[char] || 0)+1
-                return acc
-            },{})
-        return JSON.stringify(charCount(s1)===JSON.stringify(charCount(s2)))
-    }  
-    if (haveSameChars(s1,s2)) return false
+    const map1 =getFrequencyMap(s1)
+    const map2 =getFrequencyMap(s2)
+    for (const [char, count] of map1) {
+        if (map2.get(char) !== count) return false
+    }
     for (let i = 0; i<n; i++){
-        if (s1[i]===s2[i]){
+        if (s1[i]!=s2[i]){
             count_diff++
         }
     }
     if (count_diff>2) return false
     return true
 };
+
+
+console.log(areAlmostEqual(s1 = "caa", s2 = "aaz"))
