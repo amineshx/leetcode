9653db0826f02eccc9e5f93ee3d5b582795ab7e1
amineshx <amine.charef@etu.usthb.dz>
2025-02-05 15:27:03 +0100

    Update 1790. Check if One String Swap Can Make Strings Equal.js

--- 1790. Check if One String Swap Can Make Strings Equal/1790. Check if One String Swap Can Make Strings Equal.js
+++ 1790. Check if One String Swap Can Make Strings Equal/1790. Check if One String Swap Can Make Strings Equal.js
@@ -4,5 +4,22 @@
  * @return {boolean}
  */
 var areAlmostEqual = function(s1, s2) {
-    
+    const n = s1.length
+    let count_diff = 0
+    const haveSameChars = (s1,s2) =>{
+        const charCount = (str) =>
+            str.split("").reduce((acc, char)=>{
+                acc[char]=(acc[char] || 0)+1
+                return acc
+            },{})
+        return JSON.stringify(charCount(s1)===JSON.stringify(charCount(s2)))
+    }  
+    if (haveSameChars(s1,s2)) return false
+    for (let i = 0; i<n; i++){
+        if (s1[i]===s2[i]){
+            count_diff++
+        }
+    }
+    if (count_diff>2) return false
+    return true
 };
