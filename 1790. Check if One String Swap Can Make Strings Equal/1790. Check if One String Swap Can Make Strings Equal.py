3e1f703d4275d586b0bae139932f07222487406b
amineshx <amine.charef@etu.usthb.dz>
2025-02-05 15:13:00 +0100

    Update 1790. Check if One String Swap Can Make Strings Equal.py

--- 1790. Check if One String Swap Can Make Strings Equal/1790. Check if One String Swap Can Make Strings Equal.py
+++ 1790. Check if One String Swap Can Make Strings Equal/1790. Check if One String Swap Can Make Strings Equal.py
@@ -1,3 +1,15 @@
+from collections import Counter
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        count_diff=0
+        n=len(s1)
+        if Counter(s1) != Counter(s2):
+            return False 
+        for i in range(n):
+            if s1[i]!=s2[i]:
+                count_diff+=1
         
+        if count_diff>2 or count_diff==1 :
+            return False
+
+        return True
