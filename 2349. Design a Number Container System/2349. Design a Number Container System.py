1fa8a7966858552ea3c993017156c6e2e0e72939
amineshx <amine.charef@etu.usthb.dz>
2025-02-08 19:14:18 +0100

    Update 2349. Design a Number Container System.py

---
+++ 2349. Design a Number Container System/2349. Design a Number Container System.py
@@ -0,0 +1,17 @@
+class NumberContainers:
+
+    def __init__(self):
+        pass
+
+    def change(self, index: int, number: int) -> None:
+        pass
+
+    def find(self, number: int) -> int:
+        pass
+        
+
+
+# Your NumberContainers object will be instantiated and called as such:
+# obj = NumberContainers()
+# obj.change(index,number)
+# param_2 = obj.find(number)
