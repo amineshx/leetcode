8ef24404f8d466285a786f51e8983739f642691f
amineshx <amine.charef@etu.usthb.dz>
2025-02-08 19:16:56 +0100

    Update 2349. Design a Number Container System.py

--- 2349. Design a Number Container System/2349. Design a Number Container System.py
+++ 2349. Design a Number Container System/2349. Design a Number Container System.py
@@ -1,14 +1,28 @@
+from heapq import heappop, heappush
 class NumberContainers:
-
     def __init__(self):
-        pass
-
+        self.index_val = {}  
+        self.res = {}  
     def change(self, index: int, number: int) -> None:
-        pass
+        if index in self.index_val:
+            prevNum = self.index_val[index]
+            if prevNum == number:
+                return
+
+        self.index_val[index] = number
+        
+        if number not in self.res:
+            self.res[number] = []
+        heappush(self.res[number], index)
 
     def find(self, number: int) -> int:
-        pass
+        if number not in self.res:
+            return -1
         
+        while self.res[number] and self.index_val[self.res[number][0]] != number:
+            heappop(self.res[number])
+
+        return self.res[number][0] if self.res[number] else -1
 
 
 # Your NumberContainers object will be instantiated and called as such:
