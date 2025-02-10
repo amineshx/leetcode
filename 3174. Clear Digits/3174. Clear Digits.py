3041cc140fff26fb9d33cb06a6adc9745fecff4f
amineshx <amine.charef@etu.usthb.dz>
2025-02-10 22:51:14 +0100

    Update 3174. Clear Digits.py

--- 3174. Clear Digits/3174. Clear Digits.py
+++ 3174. Clear Digits/3174. Clear Digits.py
@@ -1,3 +1,17 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
-        
+        res=[]
+
+        for char in s:
+            
+            print("char",char," res",res)
+            if (ord(char)>122 or ord(char)<97) and res:
+                res.pop()
+                continue
+            res.append(char)
+                
+                
+        return ''.join(res)
+
+sol = Solution()
+print(sol.clearDigits("fdd14"))
