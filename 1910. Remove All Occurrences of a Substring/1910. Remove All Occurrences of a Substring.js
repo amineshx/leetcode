5ba0e8715b33bafd22ffd12c1ffa371298158c04
amineshx <amine.charef@etu.usthb.dz>
2025-02-11 23:01:20 +0100

    Update 1910. Remove All Occurrences of a Substring.js

--- 1910. Remove All Occurrences of a Substring/1910. Remove All Occurrences of a Substring.js
+++ 1910. Remove All Occurrences of a Substring/1910. Remove All Occurrences of a Substring.js
@@ -4,5 +4,17 @@
  * @return {string}
  */
 var removeOccurrences = function(s, part) {
-    
+  let stack = []
+  for (let char of s) {
+    stack.push(char)
+    if (stack.length>=part.length){
+        const isPart = stack.slice(-part.length).join("")
+        if(isPart===part){
+            for(let _ =0 ; _<part.length;_++){
+                stack.pop()
+            }
+        }
+    }
+  } 
+  return stack.join("")
 };
