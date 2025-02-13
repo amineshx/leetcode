24781e0ebfa71393c3df837ba07ca48ef297b1a0
amineshx <amine.charef@etu.usthb.dz>
2025-02-13 01:49:17 +0100

    Update 3066. Minimum Operations to Exceed Threshold Value II.py

--- 3066. Minimum Operations to Exceed Threshold Value II/3066. Minimum Operations to Exceed Threshold Value II.py
+++ 3066. Minimum Operations to Exceed Threshold Value II/3066. Minimum Operations to Exceed Threshold Value II.py
@@ -1,4 +1,14 @@
 from typing import List
+import bisect
+from collections import deque
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        
+        nums.sort()
+        q = deque(nums)
+        res=0
+        while q[0]<k:
+            x,y=q.popleft(),q.popleft()
+            num = min(x,y) *2 + max(x,y)
+            bisect.insort(q,num)
+            res+=1
+        return res
