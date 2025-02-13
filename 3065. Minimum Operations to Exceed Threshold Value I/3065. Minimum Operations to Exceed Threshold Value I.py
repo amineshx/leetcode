2a14dd7301bfd6b35f0f233b0f4d22f224066d02
amineshx <amine.charef@etu.usthb.dz>
2025-02-13 01:37:08 +0100

    Update 3065. Minimum Operations to Exceed Threshold Value I.py

--- 3065. Minimum Operations to Exceed Threshold Value I/3065. Minimum Operations to Exceed Threshold Value I.py
+++ 3065. Minimum Operations to Exceed Threshold Value I/3065. Minimum Operations to Exceed Threshold Value I.py
@@ -1,4 +1,17 @@
 from typing import List
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (right + left) // 2
+            if nums[mid] < k:
+                left = mid + 1  
+            else:
+                right = mid - 1  
         
+        return left  
+
+sol = Solution()
+print(sol.minOperations(nums = [1, 1, 2, 4, 9], k = 9))  
