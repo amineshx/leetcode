a010922c62f52715f9e64776f10bc7d07b6f63b8
amineshx <amine.charef@etu.usthb.dz>
2025-02-14 01:20:44 +0100

    Update 1352. Product of the Last K Numbers.py

---
+++ 1352. Product of the Last K Numbers/1352. Product of the Last K Numbers.py
@@ -0,0 +1,16 @@
+class ProductOfNumbers:
+
+    def __init__(self):
+        pass
+
+    def add(self, num: int) -> None:
+        pass
+
+    def getProduct(self, k: int) -> int:
+        pass
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
