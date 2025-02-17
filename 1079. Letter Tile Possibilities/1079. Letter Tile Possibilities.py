82ab21ab7b596df771d45be09845d7fbefa8c6e7
amineshx <amine.charef@etu.usthb.dz>
2025-02-17 22:14:16 +0100

    Update 1079. Letter Tile Possibilities.py

--- 1079. Letter Tile Possibilities/1079. Letter Tile Possibilities.py
+++ 1079. Letter Tile Possibilities/1079. Letter Tile Possibilities.py
@@ -1,3 +1,15 @@
+from collections import Counter
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
+        def backtracking(counter):
+            res = 0
+            for char in counter:
+                if counter[char]>0:
+                    res+=1
+                    counter[char]-=1
+                    res+=backtracking(counter)
+                    counter[char]+=1
+            return res
         
+        counnter = Counter(tiles)
+        return backtracking(counnter)
