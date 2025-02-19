ce718813812f4927d17d85199bb52ddcd884e9a1
amineshx <amine.charef@etu.usthb.dz>
2025-02-19 23:10:24 +0100

    Update 1415. The k-th Lexicographical String of All Happy Strings of Length n.py

--- 1415. The k-th Lexicographical String of All Happy Strings of Length n/1415. The k-th Lexicographical String of All Happy Strings of Length n.py
+++ 1415. The k-th Lexicographical String of All Happy Strings of Length n/1415. The k-th Lexicographical String of All Happy Strings of Length n.py
@@ -1,3 +1,17 @@
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
+        def dfs(prefix, n, k):
+            if n == 0:
+                return prefix
+            for c in 'abc':
+                if prefix and c == prefix[-1]:
+                    continue
+                cnt = 2 ** (n2 - len(prefix) - 1)
+                if cnt >= k:
+                    return dfs(prefix + c, n - 1, k)
+                else:
+                    k -= cnt
+            return ""
         
+        n2 = n
+        return dfs("", n, k)
