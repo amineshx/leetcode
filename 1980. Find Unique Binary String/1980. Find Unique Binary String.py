d6b9336040cbf3568073bc579c017ca6dbd188cc
amineshx <amine.charef@etu.usthb.dz>
2025-02-20 23:49:14 +0100

    Update 1980. Find Unique Binary String.py

--- 1980. Find Unique Binary String/1980. Find Unique Binary String.py
+++ 1980. Find Unique Binary String/1980. Find Unique Binary String.py
@@ -1,4 +1,19 @@
 from typing import List
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
+        strings_set = { num for num in nums}
+        def backtracking(i, current):
+            if i == len(nums):
+                res = "".join(current)
+                return None if res in strings_set else res 
+            
+            res = backtracking(i+1, current)
+            if res: 
+                return res
+            
+            current[i]='1'
+            res = backtracking(i+1, current)
+            if res:
+                return res
         
+        return backtracking(0,["0" for _ in nums])
