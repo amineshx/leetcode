6c0fadb1a8b281f041a1ed248d3ef826f0856580
amineshx <amine.charef@etu.usthb.dz>
2025-02-22 23:51:23 +0100

    Update 1028. Recover a Tree From Preorder Traversal.py

--- 1028. Recover a Tree From Preorder Traversal/1028. Recover a Tree From Preorder Traversal.py
+++ 1028. Recover a Tree From Preorder Traversal/1028. Recover a Tree From Preorder Traversal.py
@@ -7,4 +7,29 @@ class TreeNode:
         self.right = right
 class Solution:
     def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
-        
+        dash_num = 0
+        stack = []
+        i=0
+
+        while i<len(traversal):
+            if traversal[i]=='-':
+                dash_num+=1
+                i+=1
+            else:
+                j=i
+                while j< len(traversal) and traversal[j]!='-':
+                    j+=1
+                val = int(traversal[i:j])
+                node = TreeNode(val)
+
+                while len(stack)>dash_num:
+                    stack.pop()
+                
+                if stack and not stack[-1].left:
+                    stack[-1].left = node
+                elif stack:
+                    stack[-1].right = node
+                stack.append(node)
+                i=j
+                dash_num=0
+        return stack[0]
