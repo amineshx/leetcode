e9019fbdf9473ceee9b7b7c284036a99fa95f36f
amineshx <amine.charef@etu.usthb.dz>
2025-02-24 00:52:33 +0100

    Update 889. Construct Binary Tree from Preorder and Postorder Traversal.py

--- 889. Construct Binary Tree from Preorder and Postorder Traversal/889. Construct Binary Tree from Preorder and Postorder Traversal.py
+++ 889. Construct Binary Tree from Preorder and Postorder Traversal/889. Construct Binary Tree from Preorder and Postorder Traversal.py
@@ -1,4 +1,5 @@
 from typing import List , Optional
+from collections import deque   # has no relation with challenge , just to visualize the tree
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -27,3 +28,39 @@ class Solution:
 
 
         return buid_tree(0,n-1,0,n-1)
+    
+
+
+
+# Function to convert tree to array (level-order traversal)
+def tree_to_array(root: Optional[TreeNode]) -> List[Optional[int]]:
+    if not root:
+        return []
+    
+    res = []
+    q = deque([root])
+
+    while q:
+        node = q.popleft()
+
+        if node:
+            res.append(node.val)
+            q.append(node.left)
+            q.append(node.right)
+        else:
+            res.append(None)
+    
+    # Remove trailing None values
+    while res and res[-1] is None:
+        res.pop()
+    return res 
+
+sol = Solution()
+tree1 = sol.constructFromPrePost(preorder=[1,2,4,5,3,6,7], postorder=[4,5,2,6,7,3,1])
+tree2 = sol.constructFromPrePost(preorder=[1], postorder=[1])
+
+# Print tree as an array
+print(tree_to_array(tree1))  # Expected output: [1, 2, 3, 4, 5, 6, 7]
+print(tree_to_array(tree2))  # Expected output: [1]
+
+    
