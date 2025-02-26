9062bfe88239456c252468b56645c1655329f9c4
amineshx <amine.charef@etu.usthb.dz>
2025-02-26 23:27:34 +0100

    Update 1749. Maximum Absolute Sum of Any Subarray.py

--- 1749. Maximum Absolute Sum of Any Subarray/1749. Maximum Absolute Sum of Any Subarray.py
+++ 1749. Maximum Absolute Sum of Any Subarray/1749. Maximum Absolute Sum of Any Subarray.py
@@ -1,4 +1,11 @@
 from typing import List
 class Solution:
     def maxAbsoluteSum(self, nums: List[int]) -> int:
-        
+        min_prefix_sum = max_prefix_sum = current = res = 0
+
+        for num in nums:
+            current+=num
+            res = max(res, abs(current-min_prefix_sum), abs(current-max_prefix_sum))
+            max_prefix_sum = max(max_prefix_sum,current)
+            min_prefix_sum = min(min_prefix_sum, current)
+        return res
