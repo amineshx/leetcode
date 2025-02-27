bc0eb7980aa2326011ae9e07e483af1109bcf780
amineshx <amine.charef@etu.usthb.dz>
2025-02-27 23:58:47 +0100

    Update 100. Same Tree.js

--- 100. Same Tree/100. Same Tree.js
+++ 100. Same Tree/100. Same Tree.js
@@ -1,16 +1,31 @@
-
-//Definition for a binary tree node.
+// Definition for a binary tree node.
 function TreeNode(val, left, right) {
-    this.val = (val===undefined ? 0 : val)
-    this.left = (left===undefined ? null : left)
-    this.right = (right===undefined ? null : right)
+    this.val = (val === undefined ? 0 : val);
+    this.left = (left === undefined ? null : left);
+    this.right = (right === undefined ? null : right);
 }
 
 /**
-@param {TreeNode} p
-@param {TreeNode} q
-@return {boolean}
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    
+    let queueP = [p];
+    let queueQ = [q];
+
+    while (queueP.length && queueQ.length) {
+        let nodeP = queueP.shift();
+        let nodeQ = queueQ.shift();
+
+        if (!nodeP && !nodeQ) continue;
+        if (!nodeP || !nodeQ || nodeP.val !== nodeQ.val) return false;
+
+        queueP.push(nodeP.left);
+        queueQ.push(nodeQ.left);
+        queueP.push(nodeP.right);
+        queueQ.push(nodeQ.right);
+    }
+
+    return queueP.length === 0 && queueQ.length === 0;
 };
