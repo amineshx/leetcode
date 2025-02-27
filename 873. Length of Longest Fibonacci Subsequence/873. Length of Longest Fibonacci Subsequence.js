fc31b07cee3b3ee84a43b50259b425c4b36b7881
amineshx <amine.charef@etu.usthb.dz>
2025-02-28 00:50:07 +0100

    Update 873. Length of Longest Fibonacci Subsequence.js with .has() instead of in

--- 873. Length of Longest Fibonacci Subsequence/873. Length of Longest Fibonacci Subsequence.js
+++ 873. Length of Longest Fibonacci Subsequence/873. Length of Longest Fibonacci Subsequence.js
@@ -3,5 +3,23 @@
  * @return {number}
  */
 var lenLongestFibSubseq = function(arr) {
-    
+    let arr_to_set = new Set(arr)  
+    let res=0
+
+    for (let i=0; i<arr.length-1;i++){
+        for (let j=i+1; j<arr.length;j++){
+            let prev = arr[i]
+            let cur = arr[j]
+            let next = prev+cur 
+            let length = 2
+            while (arr_to_set.has(next)){
+                length++
+                prev = cur 
+                cur = next
+                next = prev+cur
+                res = Math.max(res,length)
+            }
+        }
+    }
+    return res
 };
