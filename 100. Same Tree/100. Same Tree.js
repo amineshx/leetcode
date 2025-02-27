0595626d090fc46f4175dbace4994fb263d0dbee
amineshx <amine.charef@etu.usthb.dz>
2025-02-27 23:59:51 +0100

    Update 100. Same Tree.js

--- 100. Same Tree/100. Same Tree.js
+++ 100. Same Tree/100. Same Tree.js
@@ -11,23 +11,23 @@ function TreeNode(val, left, right) {
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    let queueP = [p];
-    let queueQ = [q];
+    let queueP = [p]
+    let queueQ = [q]
 
     while (queueP.length && queueQ.length) {
-        let nodeP = queueP.shift();
-        let nodeQ = queueQ.shift();
+        let nodeP = queueP.shift()
+        let nodeQ = queueQ.shift()
 
-        if (!nodeP && !nodeQ) continue;
-        if (!nodeP || !nodeQ || nodeP.val !== nodeQ.val) return false;
+        if (!nodeP && !nodeQ) continue
+        if (!nodeP || !nodeQ || nodeP.val !== nodeQ.val) return false
 
-        queueP.push(nodeP.left);
-        queueQ.push(nodeQ.left);
-        queueP.push(nodeP.right);
-        queueQ.push(nodeQ.right);
+        queueP.push(nodeP.left)
+        queueQ.push(nodeQ.left)
+        queueP.push(nodeP.right)
+        queueQ.push(nodeQ.right)
     }
 
-    return queueP.length === 0 && queueQ.length === 0;
+    return queueP.length === 0 && queueQ.length === 0
 };
 
 function buildTree(values) {
