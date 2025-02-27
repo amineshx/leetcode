767e2d3a11a81e243d257d47603b255419c47004
amineshx <amine.charef@etu.usthb.dz>
2025-02-27 23:52:53 +0100

    Update 100. Same Tree.py

--- 100. Same Tree/100. Same Tree.py
+++ 100. Same Tree/100. Same Tree.py
@@ -1,4 +1,6 @@
 from typing import Optional
+from collections import deque  
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -7,4 +9,19 @@ class TreeNode:
         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        
+        queue_p = deque([p])  
+        queue_q = deque([q])  
+        while queue_p and queue_q:
+            node_p = queue_p.popleft()
+            node_q = queue_q.popleft()
+            if not node_p and not node_q:
+                continue
+            if not node_p or not node_q or node_q.val!=node_p.val:
+                return False
+            
+            queue_p.append(node_p.left)
+            queue_q.append(node_q.left)
+            queue_p.append(node_p.right)
+            queue_q.append(node_q.right)
+
+        return not queue_p and not queue_q
