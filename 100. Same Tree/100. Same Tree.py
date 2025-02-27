d7a7e37ca52c11aab945c1413fa07ceeb59c08e7
amineshx <amine.charef@etu.usthb.dz>
2025-02-27 23:55:19 +0100

    Update 100. Same Tree.py

--- 100. Same Tree/100. Same Tree.py
+++ 100. Same Tree/100. Same Tree.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 from collections import deque  
 
 # Definition for a binary tree node.
@@ -25,3 +25,40 @@ class Solution:
             queue_q.append(node_q.right)
 
         return not queue_p and not queue_q
+
+def build_tree(values: List[Optional[int]]) -> Optional[TreeNode]:
+    if not values:
+        return None
+
+    root = TreeNode(values[0])
+    queue = deque([root])
+    i = 1
+
+    while i < len(values):
+        node = queue.popleft()
+        if values[i] is not None:
+            node.left = TreeNode(values[i])
+            queue.append(node.left)
+        i += 1
+
+        if i < len(values) and values[i] is not None:
+            node.right = TreeNode(values[i])
+            queue.append(node.right)
+        i += 1
+
+    return root
+
+# Test Cases
+p1 = build_tree([1,2,3])
+q1 = build_tree([1,2,3])
+
+p2 = build_tree([1,2])
+q2 = build_tree([1,None,2])
+
+p3 = build_tree([1,2,1])
+q3 = build_tree([1,1,2])
+
+solution = Solution()
+print(solution.isSameTree(p1, q1))  # True
+print(solution.isSameTree(p2, q2))  # False
+print(solution.isSameTree(p3, q3)) 
