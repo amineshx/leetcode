9240f92bef6cf69a84dd91b4ec248d424c73c8c3
amineshx <amine.charef@etu.usthb.dz>
2025-02-28 21:25:46 +0100

    Update 1092. Shortest Common Supersequence.py

--- 1092. Shortest Common Supersequence/1092. Shortest Common Supersequence.py
+++ 1092. Shortest Common Supersequence/1092. Shortest Common Supersequence.py
@@ -1,3 +1,4 @@
+# sol1 memory time execeeded
 class Solution:
     def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
         memo = {}
@@ -21,4 +22,41 @@ class Solution:
             memo[(ch1,ch2)]=res2
             return res2
         return backtracking(0,0)
-            
+
+       # DP solution 
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        m, n = len(str1), len(str2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if str1[i-1] == str2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        
+        i, j = m, n
+        result = []
+        
+        while i > 0 and j > 0:
+            if str1[i-1] == str2[j-1]:
+                result.append(str1[i-1])
+                i -= 1
+                j -= 1
+            elif dp[i-1][j] > dp[i][j-1]:
+                result.append(str1[i-1])
+                i -= 1
+            else:
+                result.append(str2[j-1])
+                j -= 1
+        
+        while i > 0:
+            result.append(str1[i-1])
+            i -= 1
+        
+        while j > 0:
+            result.append(str2[j-1])
+            j -= 1
+        
+        return ''.join(result[::-1])
