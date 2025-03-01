8716cec590a27b82428b443deba1e117bfa48589
amineshx <amine.charef@etu.usthb.dz>
2025-03-01 04:16:22 +0100

    Update 2460. Apply Operations to an Array.py

--- 2460. Apply Operations to an Array/2460. Apply Operations to an Array.py
+++ 2460. Apply Operations to an Array/2460. Apply Operations to an Array.py
@@ -1,4 +1,15 @@
 from typing import List
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1]:
+                nums[i]*=2
+                nums[i+1]=0
         
+        non_zero_i=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[non_zero_i],nums[i]=nums[i],nums[non_zero_i]
+                non_zero_i+=1
+        return nums
+
