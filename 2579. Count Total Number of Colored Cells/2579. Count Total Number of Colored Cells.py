4d14f21403b4f322df9a94c189671f8fcfaf65bb
amineshx <amine.charef@etu.usthb.dz>
2025-03-05 03:39:12 +0100

    Update 2579. Count Total Number of Colored Cells.py

--- 2579. Count Total Number of Colored Cells/2579. Count Total Number of Colored Cells.py
+++ 2579. Count Total Number of Colored Cells/2579. Count Total Number of Colored Cells.py
@@ -1,3 +1,14 @@
+# O(n)
+class Solution:
+    def coloredCells(self, n: int) -> int:
+        res=1
+        for i in range(1,n):
+            res+=i*4
+        return res
+
+# O(1)
 class Solution:
     def coloredCells(self, n: int) -> int:
         
+        return (1+4*((n-1)*n)//2)
+
