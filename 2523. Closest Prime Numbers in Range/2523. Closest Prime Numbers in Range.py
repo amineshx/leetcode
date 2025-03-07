f8ae370d3ca2c8be960b8b3282b3b728788d21b0
amineshx <amine.charef@etu.usthb.dz>
2025-03-07 18:15:43 +0100

    Update 2523. Closest Prime Numbers in Range.py

--- 2523. Closest Prime Numbers in Range/2523. Closest Prime Numbers in Range.py
+++ 2523. Closest Prime Numbers in Range/2523. Closest Prime Numbers in Range.py
@@ -1,16 +1,16 @@
 from typing import List
-import math
 class Solution:
     def closestPrimes(self, left: int, right: int) -> List[int]:
-        def is_prime(num):
-            if num < 2:
-                return False
-            for i in range(2, int(math.sqrt(num)) + 1):
-                if num % i == 0:
-                    return False
-            return True
+        limit = right + 1
+        is_prime = [True] * limit
+        is_prime[0] = is_prime[1] = False  # 0 and 1 are not prime
+
+        for i in range(2, int(right**0.5) + 1):
+            if is_prime[i]:
+                for j in range(i * i, limit, i):
+                    is_prime[j] = False
         
-        primes = [num for num in range(left, right + 1) if is_prime(num)]
+        primes = [num for num in range(left, right + 1) if is_prime[num]]
         if len(primes) < 2: return [-1, -1]
         
         min_diff = float('inf')
