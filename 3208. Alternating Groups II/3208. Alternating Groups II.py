27104dbea8ca1cf9e6432cd787fc9175677c5f7a
amineshx <amine.charef@etu.usthb.dz>
2025-03-09 17:32:56 +0100

    Update 3208. Alternating Groups II

--- 3208. Alternating Groups II/3208. Alternating Groups II.py
+++ 3208. Alternating Groups II/3208. Alternating Groups II.py
@@ -1,17 +1,18 @@
 from typing import List
+
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
-        n=len(colors)
-        res=0
+        n = len(colors)
+        colors.extend(colors[:k-1])  
+        count, valid_length = 0, 1  
 
-        for i in range(n):
-            check=True
+        for i in range(1, n):
+            if colors[i] != colors[i - 1]: 
+                valid_length += 1
+            else:
+                valid_length = 1 
+            
+            if valid_length >= k:
+                count += 1
 
-            for j in range(1,k):
-                if colors[(i+j)%n]==colors[(i+j-1)%n]:
-                    check=False
-                    break
-            if check:
-                res+=1
-        
-        return res
+        return count
