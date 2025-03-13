49bfdb28c1127e9bddb0a2541c5647271450dca6
amineshx <amine.charef@etu.usthb.dz>
2025-03-14 00:33:02 +0100

    Update 3356. Zero Array Transformation I.py brute force sol TLE

--- 3356. Zero Array Transformation II/3356. Zero Array Transformation II.py
+++ 3356. Zero Array Transformation II/3356. Zero Array Transformation II.py
@@ -1,4 +1,22 @@
 from typing import List
+
+# brute force way O(n*m)
 class Solution:
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+        if all(num == 0 for num in nums):
+            return 0
+        m=len(queries)
+
+        def check_if_zero():
+            return all(num == 0 for num in nums)  
         
+        for i in range(m):
+            l, r, val = queries[i]
+            
+            for j in range(l, r + 1):
+                nums[j] = max(0, nums[j] - val)
+            
+            if check_if_zero():
+                return i + 1  
+        
+        return -1  
