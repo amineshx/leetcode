e09aa8d9b1fc97b039565166bed440ee73cc7ab4
amineshx <amine.charef@etu.usthb.dz>
2025-03-14 00:14:47 +0100

    Update 28. Find the Index of the First Occurrence in a String

--- 28. Find the Index of the First Occurrence in a String/28. Find the Index of the First Occurrence in a String.py
+++ 28. Find the Index of the First Occurrence in a String/28. Find the Index of the First Occurrence in a String.py
@@ -1,6 +1,13 @@
+
+# sliding window "neive algorithm " O(len(haystack)*len(needle))
 class Solution:
     def strStr(self, haystack, needle):
         for i in range(len(haystack) - len(needle) + 1):
             if haystack[i:i+len(needle)] == needle:
                 return i
         return -1
+
+# KMP o(len(haystack)+len(needle))
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        
