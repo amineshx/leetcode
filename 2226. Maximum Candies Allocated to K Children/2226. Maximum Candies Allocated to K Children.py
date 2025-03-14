fe5deeb00203580feb27f65d28efcfc8ed3c5ed1
amineshx <amine.charef@etu.usthb.dz>
2025-03-14 19:50:43 +0100

    Update 2226. Maximum Candies Allocated to K Children.py

--- 2226. Maximum Candies Allocated to K Children/2226. Maximum Candies Allocated to K Children.py
+++ 2226. Maximum Candies Allocated to K Children/2226. Maximum Candies Allocated to K Children.py
@@ -1,4 +1,20 @@
 from typing import List
 class Solution:
     def maximumCandies(self, candies: List[int], k: int) -> int:
+        if sum(candies) < k: 
+            return 0
+        left=1
+        right= max(candies)
+        res = 0
+
+        while left<=right:
+            mid=(left+right)//2
+            child_cnt = sum(c // mid for c in candies)
+
+            if child_cnt>=k:
+                res=mid
+                left=mid+1
+            else:
+                right=mid-1
         
+        return res
