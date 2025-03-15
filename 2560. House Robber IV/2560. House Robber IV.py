8a6ed6666dcdd167224f90920e11cf9f35cad182
amineshx <amine.charef@etu.usthb.dz>
2025-03-15 23:38:40 +0100

    Create 2560. House Robber IV/2560. House Robber IV.py

---
+++ 2560. House Robber IV/2560. House Robber IV.py
@@ -0,0 +1,23 @@
+from typing import List
+class Solution:
+    def minCapability(self, nums: List[int], k: int) -> int:
+        def canRob(nums, mid, k):
+            count = 0
+            n = len(nums)
+            i = 0
+            while i < n:
+                if nums[i] <= mid:
+                    count += 1
+                    i += 1
+                i += 1
+            return count >= k
+        left, right = 1, max(nums)
+        res = right
+        while left <= right:
+            mid = (left + right) // 2
+            if canRob(nums, mid, k):
+                res = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        return res
