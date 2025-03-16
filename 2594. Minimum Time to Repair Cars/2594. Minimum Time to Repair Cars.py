41d47ce4d928e930368c0184f684482855ca541c
amineshx <amine.charef@etu.usthb.dz>
2025-03-17 00:27:56 +0100

    Update 2594. Minimum Time to Repair Cars.py

--- 2594. Minimum Time to Repair Cars/2594. Minimum Time to Repair Cars.py
+++ 2594. Minimum Time to Repair Cars/2594. Minimum Time to Repair Cars.py
@@ -1,4 +1,22 @@
 from typing import List
+from math import sqrt
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
+        def canRepaireCarInTime(time):
+            totale_cars = 0
+            for rank in ranks:
+                totale_cars += int(sqrt(time // rank))
+                if totale_cars>=cars:
+                    return True
+            return totale_cars >=cars
+
+        lower_time=0
+        higher_time=max(ranks)*cars**2
+
+        while lower_time<higher_time:
+            mid = (lower_time+higher_time)//2
+            if canRepaireCarInTime(mid):
+                higher_time=mid
+            else: lower_time=mid+1
         
+        return lower_time
