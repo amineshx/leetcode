9f8bcf3dc0ad8826238502783b2a24cadcef85fc
amineshx <amine.charef@etu.usthb.dz>
2025-03-21 02:01:07 +0100

    2115. Find All Possible Recipes from Given Supplies

---
+++ 2115. Find All Possible Recipes from Given Supplies/2115. Find All Possible Recipes from Given Supplies.py
@@ -0,0 +1,38 @@
+from typing import List
+from collections import deque
+
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        available = set(supplies)
+        
+        ingredient_to_recipes = {}
+        for recipe, ing_list in zip(recipes, ingredients):
+            for ing in ing_list:
+                if ing not in ingredient_to_recipes:
+                    ingredient_to_recipes[ing] = []
+                ingredient_to_recipes[ing].append(recipe)
+        
+        in_degree = {}
+        for recipe, ing_list in zip(recipes, ingredients):
+            missing = [ing for ing in ing_list if ing not in available]
+            in_degree[recipe] = len(missing)
+        
+        queue = deque()
+        for recipe in recipes:
+            if in_degree[recipe] == 0:  
+                queue.append(recipe)
+        
+        res = []
+        
+        while queue:
+            recipe = queue.popleft()
+            res.append(recipe)  
+            available.add(recipe)   
+            
+            if recipe in ingredient_to_recipes:
+                for dependent_recipe in ingredient_to_recipes[recipe]:
+                    in_degree[dependent_recipe] -= 1  
+                    if in_degree[dependent_recipe] == 0:  
+                        queue.append(dependent_recipe)
+        
+        return res
