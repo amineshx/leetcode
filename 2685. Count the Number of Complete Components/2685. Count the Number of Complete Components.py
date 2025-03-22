7f0b16b00a382839608ea8f9e5f86fe697698eda
amineshx <amine.charef@etu.usthb.dz>
2025-03-23 00:16:55 +0100

    Update 2685. Count the Number of Complete Components/2685. Count the Number of Complete Components.py dfs sol

--- 2685. Count the Number of Complete Components/2685. Count the Number of Complete Components.py
+++ 2685. Count the Number of Complete Components/2685. Count the Number of Complete Components.py
@@ -1,5 +1,5 @@
 from typing import List
-
+from collections import defaultdict
 # Union find solution 
 class Solution:
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
@@ -51,7 +51,39 @@ class Solution:
                 complete_count += 1
         
         return complete_count
+
+
+#sol2 DFS
+
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        def dfs(vortex, res):
+            if vortex in visited:
+                return
+            visited.add(vortex)
+            res.append(vortex)
+
+            for nei in adj[vortex]:
+                dfs(nei,res)
+            return res
         
+        adj = defaultdict(list)
+        for vortex1, vortex2 in edges:
+            adj[vortex1].append(vortex2)
+            adj[vortex2].append(vortex1)       
+
+        res = 0
+        visited = set()
+        for v in range(n):
+            if v in visited:
+                continue
+            component = dfs(v,[])
+            if all([len(component)-1 == len(adj[vort]) for vort in component]): 
+                res+=1
+        
+        return res
+
+
 sol = Solution()
 print(sol.countCompleteComponents(n = 6, edges = [[0,1],[0,2],[1,2],[3,4]]))
 print(sol.countCompleteComponents(n = 6, edges = [[0,1],[0,2],[1,2],[3,4],[3,5]]))
