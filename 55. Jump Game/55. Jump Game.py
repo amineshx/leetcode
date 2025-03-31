b82d94d37d48652359af869bc0bccd87efe985e3
amineshx <amine.charef@etu.usthb.dz>
2025-04-01 00:35:26 +0100

    Update 55. Jump Game.py

--- 55. Jump Game/55. Jump Game.py
+++ 55. Jump Game/55. Jump Game.py
@@ -1,4 +1,13 @@
 from typing import List
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        
+        goal = len(nums)-1
+        for i in reversed(range(len(nums)-1)):
+            if nums[i]+i>=goal:
+                goal=i 
+        if goal==0:
+            return True 
+        return False
+
+sol = Solution()
+print(sol.canJump(nums = [2,3,1,1,4]))
