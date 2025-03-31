86e212cc9c6dfefd43178b2b60b7e0d8f19d12ce
amineshx <amine.charef@etu.usthb.dz>
2025-03-31 01:15:24 +0100

    Update 763. Partition Labels/763. Partition Labels.py

--- 763. Partition Labels/763. Partition Labels.py
+++ 763. Partition Labels/763. Partition Labels.py
@@ -1,4 +1,22 @@
 from typing import List
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
+        last_occ = {}
+        for idx,char in enumerate(s):
+            last_occ[char]=idx 
         
+        max_last = 0
+        res = []
+        count=0
+        for i,c in enumerate(s):
+            if max_last < last_occ[c]:
+                max_last=last_occ[c]    
+            count+=1
+            if i==max_last:
+                res.append(count)
+                count=0
+        return res
+    
+sol = Solution()
+print(sol.partitionLabels(s = "ababcbacadefegdehijhklij"))
+print(sol.partitionLabels(s = "eccbbbbdec"))
