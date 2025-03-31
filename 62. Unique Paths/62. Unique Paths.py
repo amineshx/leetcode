4d9f5c39dcb297c877adecb978ab5e1aa94dc43e
amineshx <amine.charef@etu.usthb.dz>
2025-03-31 04:52:53 +0100

    Update 62. Unique Paths.py

--- 62. Unique Paths/62. Unique Paths.py
+++ 62. Unique Paths/62. Unique Paths.py
@@ -1,3 +1,14 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        
+        table = [[0]*n for _ in range(m)]
+        for i in range(m):
+            table[i][0]=1
+        for i in range(n):
+            table[0][i]=1
+        for i in range(1,m):
+            for j in range(1,n):
+                table[i][j]=table[i-1][j]+table[i][j-1]
+        return table[m-1][n-1]
+
+sol = Solution()
+print(sol.uniquePaths(3,7))
