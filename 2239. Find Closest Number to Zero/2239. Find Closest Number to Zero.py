a144c35ec7c59be820f5c642235bffba4c5fd75d
amineshx <amine.charef@etu.usthb.dz>
2025-03-31 02:05:56 +0100

    2239. Find Closest Number to Zero binary search solution

---
+++ 2239. Find Closest Number to Zero/2239. Find Closest Number to Zero.py
@@ -0,0 +1,22 @@
+from typing import List
+
+#binary search solution
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        nums.sort()  
+        left, right = 0, len(nums) - 1        
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] < 0:
+                left = mid + 1  
+            else:
+                right = mid  
+
+        pos = nums[left] if left < len(nums) else float('inf')
+        neg = nums[left - 1] if left > 0 else float('-inf')
+
+        return pos if abs(pos) < abs(neg) or (abs(pos) == abs(neg) and pos > neg) else neg
+
+
+sol = Solution()
+print(sol.findClosestNumber(nums = [-4,-2,1,4,8]))
