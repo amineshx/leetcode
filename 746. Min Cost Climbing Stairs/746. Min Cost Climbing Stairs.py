b4f3d37b4e1df411ea28963d7aef51f0f518e4e9
amineshx <amine.charef@etu.usthb.dz>
2025-03-31 03:40:08 +0100

    Update 746. Min Cost Climbing Stairs.py

--- 746. Min Cost Climbing Stairs/746. Min Cost Climbing Stairs.py
+++ 746. Min Cost Climbing Stairs/746. Min Cost Climbing Stairs.py
@@ -1,4 +1,6 @@
 from typing import List
+
+#memo
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         
@@ -10,3 +12,32 @@ class Solution:
             return memo[n]
         
         return min(dp(len(cost)-1,memo={}),dp(len(cost)-2,memo={}))
+
+#tabulation
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n=len(cost)
+        dp=[0]*n 
+        dp[0]=cost[0]
+        dp[1]=cost[1]
+
+        for i in range(2,n):
+            dp[i]=cost[i]+min(dp[i-1],dp[i-2])
+        return min(dp[n-1],dp[n-2])
+
+# liniar sol
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        prev2, prev1 = cost[0], cost[1]
+        
+        for i in range(2, len(cost)):
+            curr = cost[i] + min(prev1, prev2)
+            prev2, prev1 = prev1, curr
+
+        return min(prev1, prev2)
+
+
+
+sol = Solution()
+print(sol.minCostClimbingStairs([10, 15, 20]))  # Output: 15
+print(sol.minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))  # Output: 6
