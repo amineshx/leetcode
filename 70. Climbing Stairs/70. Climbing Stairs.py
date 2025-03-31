918dcc0e6691a004b4398a23c937d899e6b8b7d5
amineshx <amine.charef@etu.usthb.dz>
2025-03-31 03:06:47 +0100

    Update 70. Climbing Stairs.py with memo

--- 70. Climbing Stairs/70. Climbing Stairs.py
+++ 70. Climbing Stairs/70. Climbing Stairs.py
@@ -1,3 +1,4 @@
+#tabulation
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1:
@@ -11,3 +12,12 @@ class Solution:
             dp[i] = dp[i-1] + dp[i-2]
         
         return dp[n]
+
+#memo
+class Solution:
+    def climbStairs(self, n: int,memo={}) -> int:
+        if n==1:return 1
+        if n==2: return 2
+        if n in memo: return memo[n]
+        memo[n]=self.climbStairs(n-1,memo)+self.climbStairs(n-2,memo)
+        return memo[n]
