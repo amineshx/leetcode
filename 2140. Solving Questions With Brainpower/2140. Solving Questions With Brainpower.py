c53ed0db0d650df147f7c078d472af532242da5e
amineshx <amine.charef@etu.usthb.dz>
2025-04-03 00:44:22 +0100

    Update 2140. Solving Questions With Brainpower.py with backtracking and memo solution

--- 2140. Solving Questions With Brainpower/2140. Solving Questions With Brainpower.py
+++ 2140. Solving Questions With Brainpower/2140. Solving Questions With Brainpower.py
@@ -2,3 +2,13 @@ from typing import List
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
         
+        def backtrack(i,memo):
+            if i>=len(questions):
+                return 0
+            if i in memo : return memo[i]
+
+            points,brainpower = questions[i]
+            memo[i] = max(backtrack(i+1,memo),points+backtrack(i+1+brainpower,memo))
+            return memo[i]
+        
+        return backtrack(0,memo={})
