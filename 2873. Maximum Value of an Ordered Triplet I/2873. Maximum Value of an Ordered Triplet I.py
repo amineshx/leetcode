ffc66ffa55cdbb1c72a6bfca76be88d7fb74e9e8
amineshx <amine.charef@etu.usthb.dz>
2025-04-03 00:29:58 +0100

    Update 2873. Maximum Value of an Ordered Triplet I

--- 2873. Maximum Value of an Ordered Triplet I/2873. Maximum Value of an Ordered Triplet I.py
+++ 2873. Maximum Value of an Ordered Triplet I/2873. Maximum Value of an Ordered Triplet I.py
@@ -1,4 +1,10 @@
 from typing import List
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
-        
+        res = 0
+        for i in range(len(nums)-2):
+            for j in range(i+1,len(nums)-1):
+                for k in range(j+1,len(nums)):
+                    op = (nums[i]-nums[j])*nums[k]
+                    res = max(op,res)
+        return res
