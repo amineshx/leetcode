3436ff486b78fe21b139ef51d0b9d9cb32915ba7
amineshx <amine.charef@etu.usthb.dz>
2025-04-03 01:39:44 +0100

    Update 2873. Maximum Value of an Ordered Triplet II not working solition O(n) TLE

--- 2874. Maximum Value of an Ordered Triplet II/2874. Maximum Value of an Ordered Triplet II.py
+++ 2874. Maximum Value of an Ordered Triplet II/2874. Maximum Value of an Ordered Triplet II.py
@@ -1,4 +1,15 @@
 from typing import List
+
+# O(n^2) 
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
-        
+        res = 0
+        left = nums[0]
+        for j in range(1,len(nums)-1):
+            if left<nums[j]:
+                left=nums[j]
+                continue
+            for k in range(j+1,len(nums)):
+                op = (left-nums[j])*nums[k]
+                res = max(op,res)
+        return res
