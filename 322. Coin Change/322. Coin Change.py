126d23d12a0637f07540fd482502958145931be6
amineshx <amine.charef@etu.usthb.dz>
2025-04-03 17:50:11 +0100

    Update 322. Coin Change.py

--- 322. Coin Change/322. Coin Change.py
+++ 322. Coin Change/322. Coin Change.py
@@ -1,4 +1,16 @@
 from typing import List
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+
+        for c in coins:  
+            for i in range(c, amount + 1):  
+                dp[i] = min(dp[i], dp[i - c] + 1)  
+
+        return dp[amount] if dp[amount] != float('inf') else -1
         
+sol = Solution()
+print(sol.coinChange(coins = [1,2,5], amount = 11))
+print(sol.coinChange(coins = [2], amount = 3))
+print(sol.coinChange(coins = [1], amount = 0))
