4bcbda7907b8d51eebb31be559aec3fb5e19a3c6
amineshx <amine.charef@etu.usthb.dz>
2025-04-07 23:57:07 +0100

    Update 1123. Lowest Common Ancestor of Deepest Leaves.py

--- 1123. Lowest Common Ancestor of Deepest Leaves/1123. Lowest Common Ancestor of Deepest Leaves.py
+++ 1123. Lowest Common Ancestor of Deepest Leaves/1123. Lowest Common Ancestor of Deepest Leaves.py
@@ -7,4 +7,19 @@ class TreeNode:
         self.right = right
 class Solution:
     def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        
+        def dfs(node, depth):
+            if not node:
+                return (None, depth)
+            
+            left_lca, left_depth = dfs(node.left, depth + 1)
+            right_lca, right_depth = dfs(node.right, depth + 1)
+
+            if left_depth > right_depth:
+                return (left_lca, left_depth)
+            elif right_depth > left_depth:
+                return (right_lca, right_depth)
+            else:
+                return (node, left_depth)
+
+        lca_node, _ = dfs(root, 0)
+        return lca_node
