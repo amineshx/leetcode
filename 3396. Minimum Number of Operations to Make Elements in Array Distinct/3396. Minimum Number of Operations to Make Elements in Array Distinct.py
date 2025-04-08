bca7b91b839b8c0ccd7c6e21554c809ab4b3fc92
amineshx <amine.charef@etu.usthb.dz>
2025-04-08 16:37:21 +0100

    Update 3396. Minimum Number of Operations to Make Elements in Array Distinct.py

--- 3396. Minimum Number of Operations to Make Elements in Array Distinct/3396. Minimum Number of Operations to Make Elements in Array Distinct.py
+++ 3396. Minimum Number of Operations to Make Elements in Array Distinct/3396. Minimum Number of Operations to Make Elements in Array Distinct.py
@@ -1,4 +1,13 @@
 from typing import List
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        
+        operations = 0
+        while len(nums) != len(set(nums)):
+            nums = nums[3:]
+            operations += 1
+        return operations
+
+
+
+sol = Solution()
+print(sol.minimumOperations(nums = [1,2,3,4,2,3,3,5,7]))
