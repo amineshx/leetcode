1008609d58e98c544127a2f80e63d34c3581575c
amineshx <amine.charef@etu.usthb.dz>
2025-04-09 23:05:30 +0100

    Update 3375. Minimum Operations to Make Array Values Equal to K

--- 3375. Minimum Operations to Make Array Values Equal to K/3375. Minimum Operations to Make Array Values Equal to K.py
+++ 3375. Minimum Operations to Make Array Values Equal to K/3375. Minimum Operations to Make Array Values Equal to K.py
@@ -1,4 +1,11 @@
 from typing import List
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
+        if k>min(nums):
+            return -1
         
+        uniq_nums = set(nums)
+        if k in uniq_nums:
+            return len(uniq_nums)-1
+        else:
+            return len(uniq_nums)
