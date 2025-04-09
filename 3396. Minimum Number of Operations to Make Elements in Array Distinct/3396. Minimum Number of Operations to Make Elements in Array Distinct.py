c907ac91d983f61e496731b9887c0bc8eaa21ae9
amineshx <amine.charef@etu.usthb.dz>
2025-04-09 22:43:29 +0100

    Create 3375. Minimum Operations to Make Array Values Equal to K

--- 3396. Minimum Number of Operations to Make Elements in Array Distinct/3396. Minimum Number of Operations to Make Elements in Array Distinct.py
+++ 3396. Minimum Number of Operations to Make Elements in Array Distinct/3396. Minimum Number of Operations to Make Elements in Array Distinct.py
@@ -3,6 +3,8 @@ class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         operations = 0
         while len(nums) != len(set(nums)):
+            print(f"nums {nums}")
+            print(f"setnums {set(nums)}")
             nums = nums[3:]
             operations += 1
         return operations
