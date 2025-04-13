3eb32e9ac97a7316364e9dce78aa4c205a2b67f1
amineshx <amine.charef@etu.usthb.dz>
2025-04-13 23:24:02 +0100

    Update 2843. Count Symmetric Integers/2843. Count Symmetric Integers.py

--- 2843. Count Symmetric Integers/2843. Count Symmetric Integers.py
+++ 2843. Count Symmetric Integers/2843. Count Symmetric Integers.py
@@ -1,2 +1,18 @@
 class Solution:
     def countSymmetricIntegers(self, low: int, high: int) -> int:
+        res = 0
+        for num in range(low, high+1):
+            s=str(num)
+            n=len(s)
+
+            if n%2 !=0:
+                continue 
+
+            mid = n//2
+            left = sum(int(s[i]) for i in range(mid))
+            right = sum(int(s[i]) for i in range(mid, n))
+
+            if left == right:
+                res+=1
+        return res
+        
