b27052dbdcb2210084d9bf17beb9a3a5c9b36dc7
amineshx <amine.charef@etu.usthb.dz>
2025-07-01 02:48:12 +0100

    Update 3442. Maximum Difference Between Even and Odd Frequency I.py

--- 3442. Maximum Difference Between Even and Odd Frequency I/3442. Maximum Difference Between Even and Odd Frequency I.py
+++ 3442. Maximum Difference Between Even and Odd Frequency I/3442. Maximum Difference Between Even and Odd Frequency I.py
@@ -1,3 +1,23 @@
 class Solution:
     def maxDifference(self, s: str) -> int:
-        
+        hashmap = {}
+        for char in s:
+            if char in hashmap:
+                hashmap[char]+=1 
+            else :
+                hashmap[char]=1
+        odds = []
+        evens = []
+        for val in hashmap.values():
+            if val%2==0:
+                evens.append(val)
+            else:
+                odds.append(val)
+        res = - float('inf')
+        for o in odds:
+            for e in evens:
+                res = max(res,o-e)
+        return res
+
+sol = Solution()
+print(sol.maxDifference(s = "aaaaabbc"))
