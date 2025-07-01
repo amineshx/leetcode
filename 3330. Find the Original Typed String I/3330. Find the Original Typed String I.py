62b411e31985032db8e1da01b991430fee0ba75f
amineshx <amine.charef@etu.usthb.dz>
2025-07-01 02:18:27 +0100

    Update 3330. Find the Original Typed String I.py

--- 3330. Find the Original Typed String I/3330. Find the Original Typed String I.py
+++ 3330. Find the Original Typed String I/3330. Find the Original Typed String I.py
@@ -1,3 +1,9 @@
 class Solution:
     def possibleStringCount(self, word: str) -> int:
-        
+        res = 0
+        i=0
+        while i<len(word)-1:
+            if word[i]==word[i+1]:
+                res+=1
+            i+=1
+        return res+1
