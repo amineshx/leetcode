b966970ae458251b3cfea2946534d834bc234fff
amineshx <amine.charef@etu.usthb.dz>
2025-07-02 01:33:47 +0100

    Update 121. Best Time to Buy and Sell Stock

--- 121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.py
+++ 121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.py
@@ -7,3 +7,7 @@ class Solution:
             min_price = min(min_price,price)
             max_profit = max(max_profit, price-min_price)
         return max_profit
+
+sol = Solution()
+print(sol.maxProfit(prices = [7,1,5,3,6,4]))
+print(sol.maxProfit(prices = [7,6,4,3,1]))
