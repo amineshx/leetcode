5977c8cfd5086a9f2abe321f60db325d19b5f375
amineshx <amine.charef@etu.usthb.dz>
2025-07-03 01:17:02 +0100

    Update 3304. Find the K-th Character in String Game I

--- 3304. Find the K-th Character in String Game I/3304. Find the K-th Character in String Game I.py
+++ 3304. Find the K-th Character in String Game I/3304. Find the K-th Character in String Game I.py
@@ -2,4 +2,7 @@ from string import ascii_lowercase
 class Solution:
     def kthCharacter(self, k: int) -> str:
         return ascii_lowercase[(k-1).bit_count()]
-        
+
+sol = Solution()
+print(sol.kthCharacter(k=5))
+print(sol.kthCharacter(k=10))
