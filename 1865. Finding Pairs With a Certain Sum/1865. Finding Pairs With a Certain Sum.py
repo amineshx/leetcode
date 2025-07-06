9898f2579875fe9a7f5767627550c0738198f72d
amineshx <amine.charef@etu.usthb.dz>
2025-07-06 23:28:18 +0100

    Create 1865. Finding Pairs With a Certain Sum

---
+++ 1865. Finding Pairs With a Certain Sum/1865. Finding Pairs With a Certain Sum.py
@@ -0,0 +1,17 @@
+from typing import List
+class FindSumPairs:
+
+    def __init__(self, nums1: List[int], nums2: List[int]):
+        pass
+
+    def add(self, index: int, val: int) -> None:
+        pass
+
+    def count(self, tot: int) -> int:
+        pass
+
+
+# Your FindSumPairs object will be instantiated and called as such:
+# obj = FindSumPairs(nums1, nums2)
+# obj.add(index,val)
+# param_2 = obj.count(tot)
